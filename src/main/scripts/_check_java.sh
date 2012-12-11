68237a57122b053c53ccdd5f28993bffd9cd976c
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-11 19:50:52 +0000

    Enforce Java 1.6 or higher

---
+++ src/main/scripts/_check_java.sh
@@ -0,0 +1,15 @@
+# DESCRIPTION: This script will check an enforce java 1.6 or higher
+REQUIRED_MINOR_VERSION=6
+
+# grab full version string from running "java -version"
+VERSION=`java -version 2>&1 | grep "java version" | awk '{ print substr($3, 2, length($3)-2); }'`
+
+# parse out the minor version number
+MINOR_VERSION=`echo $VERSION | cut -d . -f 2`
+
+# check minor version, anything less than the required version is invalid
+if [ $MINOR_VERSION -lt $REQUIRED_MINOR_VERSION ]; then
+	echo "Invalid Java version $VERSION.  Java 1.$REQUIRED_MINOR_VERSION or higher is required."
+	echo "You can check your java version by running: java -version"
+	exit 1
+fi
--- src/main/scripts/bior_drill.sh
+++ src/main/scripts/bior_drill.sh
@@ -1,1 +1,6 @@
+# exit if any statement returns a non-zero exit code
+set -e
+
+$BIOR_LITE_HOME/bin/_check_java.sh
+
 java -cp $BIOR_LITE_HOME/conf:$BIOR_LITE_HOME/lib/* edu.mayo.bior.cli.CommandLineApp edu.mayo.bior.cli.cmd.DrillCommand $0 $@
