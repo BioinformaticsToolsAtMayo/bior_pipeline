cc6b1945abdaa5666173212cbad1280af90e365d
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-14 22:07:10 +0000

    New "man page" style help

--- src/main/java/edu/mayo/bior/cli/CommandLineApp.java
+++ src/main/java/edu/mayo/bior/cli/CommandLineApp.java
@@ -1,5 +1,11 @@
 package edu.mayo.bior.cli;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -9,7 +15,6 @@ import java.util.ResourceBundle;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
@@ -17,6 +22,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.WordUtils;
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
@@ -226,19 +232,145 @@ public class CommandLineApp {
 	 * 
 	 * @param plugin
 	 * @param opts
+	 * @param scriptName
+	 * @throws IOException 
 	 */
-	private void printHelp(CommandPlugin plugin, Options opts, String scriptName) {
-		String description = getDescription(plugin);
-		String header = description + "\n";
-		String cmdLineSyntax =  getUsage(scriptName, opts) + "\n";
-		String footer = "";
-		HelpFormatter formatter = new HelpFormatter();
-		formatter.printHelp(128, cmdLineSyntax, header, opts, footer);
+	private void printHelp(CommandPlugin plugin, Options opts, String scriptName) throws IOException {
+		
+		ResourceBundle bundle = getBundle(plugin);
+		
+		final int MAX_WIDTH = 94;
+		
+		String shortScriptName = getShortScriptName(scriptName);
+		
+		String shortDesc = indent(shortScriptName + " -- " + wrap(bundle.getString("short.description"), MAX_WIDTH), 1);
+		String longDesc = indent(wrap(bundle.getString("long.description"), MAX_WIDTH), 1);
+		String usage = indent(getUsage(shortScriptName, opts), 1);
+		
+		List<String> examples = new ArrayList<String>();
+		for (String key: bundle.keySet()) {
+			if (key.startsWith("example.")) {
+				examples.add(indent(wrap(bundle.getString(key), MAX_WIDTH), 1));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("NAME");
+		System.out.println(shortDesc);
+		System.out.println();
+		System.out.println("SYNOPSIS");
+		System.out.println(usage);
+		System.out.println();
+		System.out.println("DESCRIPTION");
+		System.out.println(longDesc);
+		System.out.println();
+		System.out.println(indent("The options are as follows:", 1));
+		System.out.println();
+		for (Object optObj: opts.getOptions()) {
+			Option opt = (Option) optObj;
+			String optStr = "-" + opt.getOpt() +", --" + opt.getLongOpt();  
+			if (opt.hasArg()) {
+				optStr += " <" + opt.getArgName() + ">";
+			}
+			System.out.println(indent(optStr, 1));
+			System.out.println(indent(wrap(opt.getDescription(), MAX_WIDTH), 2));
+			System.out.println();
+		}
+		if (examples.size() > 0) {
+			for (String example: examples) {
+				System.out.println("EXAMPLE");
+				System.out.println(example);
+				System.out.println();
+			}
+		}
 	}
 
+	/**
+	 * Wraps the given string to the specified width.  Lines that start with the
+	 * tag <NOWRAP> will not be wrapped.
+	 * 
+	 * @param str 
+	 * 			the string to wrap
+	 * @param max_width 
+	 * 			the maximum width
+	 * @return the wrapped string
+	 * @throws IOException
+	 */
+	private String wrap(String str, int max_width) throws IOException {
+		StringWriter sWtr = new StringWriter();
+		PrintWriter pWtr = new PrintWriter(sWtr);
+		
+		StringReader sRdr = new StringReader(str);
+		BufferedReader bRdr = new BufferedReader(sRdr);
+		String line = bRdr.readLine();
+		while (line != null) {
+			if (line.trim().startsWith("<NOWRAP>")) {
+				line = line.substring("<NOWRAP>".length());
+			} else {
+				line = WordUtils.wrap(line, max_width);				
+			}
+			pWtr.println(line);
+			
+			line = bRdr.readLine();
+		}		
+		
+		sWtr.close();		
+		String wrappedStr = sWtr.toString();
+
+		// chomp trailing line feed
+		if (wrappedStr.length() > 0) {
+			wrappedStr = wrappedStr.substring(0, wrappedStr.length() - 1);
+		}
+		
+		return wrappedStr;		
+	}
+	
+	/**
+	 * Does a "block" indent on the given string such that each line in the string
+	 * is indented over the specified number of indents.
+	 *  
+	 * @param str
+	 * 			the string to be indented
+	 * @param numIndents
+	 * 			number of indents to use
+	 * @return the indented string 		
+	 * @throws IOException
+	 */
+	private String indent(String str, int numIndents) throws IOException {
+
+		String indent = "";
+		for (int i=0; i < numIndents; i++) {
+			indent += "\t";
+		}
+		
+		StringWriter sWtr = new StringWriter();
+		PrintWriter pWtr = new PrintWriter(sWtr);
+		
+		StringReader sRdr = new StringReader(str);
+		BufferedReader bRdr = new BufferedReader(sRdr);
+		String line = bRdr.readLine();
+		while (line != null) {
+			
+			pWtr.print(indent);
+			pWtr.println(line);
+			
+			line = bRdr.readLine();
+		}		
+		
+		sWtr.close();		
+		String indentedStr = sWtr.toString();
+
+		// chomp trailing line feed
+		if (indentedStr.length() > 0) {
+			indentedStr = indentedStr.substring(0, indentedStr.length() - 1);
+		}
+		
+		return indentedStr;
+	}
+	
 	private String getUsage(String scriptName, Options opts) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(getShortScriptName(scriptName));
+		sb.append(scriptName);
 		Iterator<Option> optItr = opts.getOptions().iterator();
 		while (optItr.hasNext()) {
 			sb.append(" ");
@@ -262,6 +394,7 @@ public class CommandLineApp {
 		
 		return sb.toString();
 	}
+
 	
 	private static String getShortScriptName(String scriptName) {
 		int lastSeparatorPos = scriptName.lastIndexOf('/');
