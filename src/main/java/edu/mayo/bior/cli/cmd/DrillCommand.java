3f714c30a1f95bf8ff0ed07f6e6646333fea0acd
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-21 15:09:05 +0000

    adding test case for alts

--- src/main/java/edu/mayo/bior/cli/cmd/DrillCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/DrillCommand.java
@@ -14,6 +14,7 @@ public class DrillCommand implements CommandPlugin {
 
 	private static final char OPTION_KEEP_JSON = 'k';	
 	private static final char OPTION_PATH = 'p';
+        private static final char OPTION_DRILL_COLUMN = 'c';
 		
 	private UnixStreamPipeline mPipeline = new UnixStreamPipeline();
 	
@@ -32,9 +33,14 @@ public class DrillCommand implements CommandPlugin {
 			for (String value : line.getOptionValues(OPTION_PATH)) {
 				paths.add(value);
 			}
-		}		
+		}
+                
+                Integer col = -1;
+                if (line.hasOption(OPTION_DRILL_COLUMN)) {
+                    col = new Integer(line.getOptionValue(OPTION_DRILL_COLUMN));
+                }
 	
-		DrillPipe pipe = new DrillPipe(keepJSON, paths.toArray(new String[0]));
+		DrillPipe pipe = new DrillPipe(keepJSON, paths.toArray(new String[0]), col);
 		
 		mPipeline.execute(pipe);		
 	}
