25d4a787175924a41e5be551e886dc9fcaaed9ff
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-27 22:39:41 +0000

    updated test case

--- src/test/java/edu/mayo/bior/cli/func/SameVariantTCase.java
+++ src/test/java/edu/mayo/bior/cli/func/SameVariantTCase.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import org.junit.Test;
 
+import com.jayway.jsonpath.JsonPath;
+
 public class SameVariantTCase extends BaseFunctionalTest {
 	
 	 @Test
@@ -12,9 +14,8 @@ public class SameVariantTCase extends BaseFunctionalTest {
 
              String stdin = "src/test/resources/sameVariantCatalog.tsv.gz";
 
-             String jsonVariant = "{\"type\":\"gene\",\"chr\":\"17\",\"strand\":\"+\",\"minBP\":41177258,\"maxBP\":41184058,\"gene\":\"RND2\",\"gene_synonym\":\"ARHN; RHO7; RhoN\",\"note\":\"Rho family GTPase 2; Derived by automated computational analysis using gene prediction method: BestRefseq.\",\"GeneID\":\"8153\",\"HGNC\":\"18315\",\"HPRD\":\"03332\",\"MIM\":\"601555\"}";
-
-             CommandOutput out = executeScript("bior_same_variant.sh", jsonVariant, "-d", stdin);
+        String jsonVar = "BADCHR\t26960070XX\trs116645811X\tX\tY\t.\t.\t.\t{\"CHROM\":\"BADCHR\",\"POS\":\"26960070X\",\"ID\":\"rs116645811X\",\"REF\":\"X\",\"ALT\":\"Y\",\"QUAL\":\".\",\"FILTER\":\".\",\"_id\":\"rs116645811X\",\"_type\":\"variant\",\"_landmark\":\"BADCHR\",\"_refAllele\":\"X\",\"_altAlleles\":[\"Y\"],\"_minBP\":26960070X,\"_maxBP\":26960070X}";
+             CommandOutput out = executeScript("bior_same_variant.sh", jsonVar, "-d", stdin);
 
              assertEquals(out.stderr, 0, out.exit);
              assertEquals("", out.stderr);
@@ -26,7 +27,36 @@ public class SameVariantTCase extends BaseFunctionalTest {
              String[] cols = data.split("\t");
 
              // If variant is not found, results in empty json string
-             assertEquals("{}", cols[1].trim());
+             assertEquals("{}", cols[9].trim());
      }
 
+
+     @Test
+     public void testMatch() throws IOException, InterruptedException {
+
+             String stdin = "src/test/resources/sameVariantCatalog.tsv.gz";
+
+        String jsonVar = "21\t26960070\trs116645811\tG\tA\t.\t.\t.\t{\"CHROM\":\"21\",\"POS\":\"26960070\",\"ID\":\"rs116645811\",\"REF\":\"G\",\"ALT\":\"A\",\"QUAL\":\".\",\"FILTER\":\".\",\"_id\":\"rs116645811\",\"_type\":\"variant\",\"_landmark\":\"21\",\"_refAllele\":\"G\",\"_altAlleles\":[\"A\"],\"_minBP\":26960070,\"_maxBP\":26960070}";
+             CommandOutput out = executeScript("bior_same_variant.sh", jsonVar, "-d", stdin);
+
+             assertEquals(out.stderr, 0, out.exit);
+             assertEquals("", out.stderr);
+
+             String header = getHeader(out.stdout);
+
+             // pull out just data rows
+             String data = out.stdout.replace(header, "");
+             String[] cols = data.split("\t");
+
+             // If variant is not found, results in empty json string
+             //System.out.println(cols.length + cols[9]);
+             assertEquals(10, cols.length);
+             String json = cols[cols.length - 1];
+             assertEquals("21", JsonPath.compile("CHROM").read(json));
+
+        }
+
+
+	 
+	 
 }
