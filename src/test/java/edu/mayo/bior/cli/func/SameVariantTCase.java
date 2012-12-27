9f501b0f7e24db2a3063a21827f162b5e0e4631c
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-27 20:35:58 +0000

    updated test case

--- src/test/java/edu/mayo/bior/cli/func/SameVariantTCase.java
+++ src/test/java/edu/mayo/bior/cli/func/SameVariantTCase.java
@@ -1,20 +1,32 @@
 package edu.mayo.bior.cli.func;
 
-import java.io.File;
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import org.junit.Test;
 
 public class SameVariantTCase extends BaseFunctionalTest {
+	
+	 @Test
+     public void testNoMatch() throws IOException, InterruptedException {
 
-	@Test
-	public void test() throws IOException, InterruptedException {
-		String stdin = loadFile(new File("src/test/resources/testData/sameVariantCatalog.tsv.gz"));
+             String stdin = "src/test/resources/sameVariantCatalog.tsv.gz";
 
-		CommandOutput out = executeScript("bior_vcf_to_json.sh", stdin);
-		
-		String header = getHeader(out.stdout);
-		
-		System.out.println("HEADER=\n"+header);
-	}
+             String jsonVariant = "{\"type\":\"gene\",\"chr\":\"17\",\"strand\":\"+\",\"minBP\":41177258,\"maxBP\":41184058,\"gene\":\"RND2\",\"gene_synonym\":\"ARHN; RHO7; RhoN\",\"note\":\"Rho family GTPase 2; Derived by automated computational analysis using gene prediction method: BestRefseq.\",\"GeneID\":\"8153\",\"HGNC\":\"18315\",\"HPRD\":\"03332\",\"MIM\":\"601555\"}";
+
+             CommandOutput out = executeScript("bior_same_variant.sh", jsonVariant, "-d", stdin);
+
+             assertEquals(out.stderr, 0, out.exit);
+             assertEquals("", out.stderr);
+
+             String header = getHeader(out.stdout);
+
+             // pull out just data rows
+             String data = out.stdout.replace(header, "");
+             String[] cols = data.split("\t");
+
+             // If variant is not found, results in empty json string
+             assertEquals("{}", cols[1].trim());
+     }
+
 }
