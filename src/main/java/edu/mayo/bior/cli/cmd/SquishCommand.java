7d4beb8deae5f7c91c2bfc1727d28a5e51cdcc5f
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-03 15:45:29 +0000

    new squish pipe

---
+++ src/main/java/edu/mayo/bior/cli/cmd/SquishCommand.java
@@ -0,0 +1,34 @@
+package edu.mayo.bior.cli.cmd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+
+import edu.mayo.bior.cli.CommandPlugin;
+import edu.mayo.bior.pipeline.UnixStreamPipeline;
+import edu.mayo.pipes.JSON.DrillPipe;
+import edu.mayo.pipes.history.SquishPipe;
+
+public class SquishCommand implements CommandPlugin {
+
+        private static final char OPTION_COLLAPSE_COLUMN = 'c';
+		
+	private UnixStreamPipeline mPipeline = new UnixStreamPipeline();
+	
+	public void init(Properties props) throws Exception {
+	}
+
+	public void execute(CommandLine line) throws Exception {
+                
+                Integer col = 1;
+                if (line.hasOption(OPTION_COLLAPSE_COLUMN)) {
+                    col = new Integer(line.getOptionValue(OPTION_COLLAPSE_COLUMN));
+                }
+	
+		SquishPipe pipe = new SquishPipe(col);
+		
+		mPipeline.execute(pipe);		
+	}
+}
