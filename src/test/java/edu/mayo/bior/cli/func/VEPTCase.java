15a2778ccce14067e0e6524770c17344932efc2f
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-08 22:34:43 +0000

    new class.. incomplete

---
+++ src/test/java/edu/mayo/bior/cli/func/VEPTCase.java
@@ -0,0 +1,56 @@
+package edu.mayo.bior.cli.func;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.jayway.jsonpath.JsonPath;
+
+public class VEPTCase extends BaseFunctionalTest {
+
+	@Test
+	public void test() throws IOException, InterruptedException {
+		
+		String stdin = loadFile(new File("src/test/resources/tools/vep/vep.vcf"));
+
+		CommandOutput out = executeScript("bior_vep", stdin);
+
+		assertEquals(out.stderr, 0, out.exit);
+		assertEquals("", out.stderr);
+
+		String header = getHeader(out.stdout);
+		
+		/*assertEquals(
+				"##fileformat=VCFv4.0" +"\n" +
+				"##fileDate=20090805" +"\n" +
+				"##source=myImputationProgramV3.1" +"\n" +
+				"##reference=1000GenomesPilot-NCBI36" +"\n" +
+				"##phasing=partial" +"\n" +
+				"##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">" +"\n" +
+				"##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">" +"\n" +
+				"##INFO=<ID=AF,Number=.,Type=Float,Description=\"Allele Frequency\">" +"\n" +
+				"##INFO=<ID=AA,Number=1,Type=String,Description=\"Ancestral Allele\">" +"\n" +
+				"##INFO=<ID=DB,Number=0,Type=Flag,Description=\"dbSNP membership, build 129\">" +"\n" +
+				"##INFO=<ID=H2,Number=0,Type=Flag,Description=\"HapMap2 membership\">" +"\n" +
+				"#CHROM POS     ID        REF ALT    QUAL FILTER INFO"+"\tVCF2VariantPipe"+"\n", 
+				header);
+		*/
+		// pull out just data rows
+		String data = out.stdout.replace(header, "");
+		
+		// JSON should be added as last column (9th)
+		String[] cols = data.split("\t");
+		System.out.println(cols.length);
+		//assertEquals(9, cols.length);
+		
+		//String json = cols[cols.length - 1];
+
+        //assertEquals("20",			JsonPath.compile("CHROM").read(json));
+        //assertEquals("14370",		JsonPath.compile("POS").read(json));
+        //assertEquals(3,				JsonPath.compile("INFO.NS").read(json));
+	}
+
+}
