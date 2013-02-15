90ab5729b410df0e9e949cc200ffe45603bb6707
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-15 21:45:24 +0000

    Changed paths relative  to project

--- src/main/java/edu/mayo/bior/pipeline/TreatPipeline.java
+++ src/main/java/edu/mayo/bior/pipeline/TreatPipeline.java
@@ -4,17 +4,18 @@
  */
 package edu.mayo.bior.pipeline;
 
-import com.tinkerpop.pipes.transform.IdentityPipe;
+import java.io.IOException;
+import java.util.Arrays;
+
 import com.tinkerpop.pipes.util.Pipeline;
+
+import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.JSON.DrillPipe;
 import edu.mayo.pipes.JSON.tabix.OverlapPipe;
-import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
 import edu.mayo.pipes.bioinformatics.VCF2VariantPipe;
 import edu.mayo.pipes.history.HCutPipe;
 import edu.mayo.pipes.history.HistoryInPipe;
-import java.io.IOException;
-import java.util.Arrays;
 
 /**
  *
@@ -24,9 +25,9 @@ public class TreatPipeline {
     
     public static void main(String[] args) throws IOException{
         System.out.println("Welcome to treat");
-        String vcf = "/Users/m102417/workspace/bior_pipeline/src/test/resources/tools/vep/example.vcf";
+        String vcf = "src/test/resources/tools/vep/example.vcf";
         
-        String genesFile = "/data/catalogs/NCBIgene/GRCh37_p10/genes.tsv.bgz";
+        String genesFile = "src/test/resources/genes.tsv.bgz";
         String[] geneDrill = {"gene", "note"};
         int[] cut = {9};
         Pipeline p = new Pipeline(new CatPipe(),
