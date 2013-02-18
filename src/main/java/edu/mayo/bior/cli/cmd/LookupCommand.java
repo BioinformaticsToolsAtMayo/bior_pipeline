5eceb3d224be23c3bf1a47985fbc42266b03ff52
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-18 19:38:25 +0000

    bior_lookup command

--- src/main/java/edu/mayo/bior/cli/cmd/LookupCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/LookupCommand.java
@@ -9,12 +9,14 @@ import org.apache.commons.cli.CommandLine;
 import edu.mayo.bior.cli.CommandPlugin;
 import edu.mayo.bior.pipeline.UnixStreamPipeline;
 import edu.mayo.pipes.JSON.DrillPipe;
+import edu.mayo.pipes.JSON.lookup.LookupPipe;
 
 public class LookupCommand implements CommandPlugin {
 
 	private static final char OPTION_PATH = 'p';
 	private static final char OPTION_DRILL_COLUMN = 'c';
 	private static final char OPTION_KEEP_JSON = 'k';
+	private static final char INDEX_FILE = 'd';
 		
 	private UnixStreamPipeline mPipeline = new UnixStreamPipeline();
 	
@@ -34,13 +36,23 @@ public class LookupCommand implements CommandPlugin {
 				paths.add(value);
 			}
 		}
+		
+		String indexFile = "";
+		if (line.hasOption(INDEX_FILE)) {
+			indexFile  = line.getOptionValue(INDEX_FILE);
+		}
                 
-                Integer col = -1;
-                if (line.hasOption(OPTION_DRILL_COLUMN)) {
-                    col = new Integer(line.getOptionValue(OPTION_DRILL_COLUMN));
-                }
+        Integer col = -1;
+        if (line.hasOption(OPTION_DRILL_COLUMN)) {
+        	col = new Integer(line.getOptionValue(OPTION_DRILL_COLUMN));
+        }
 	
-		DrillPipe pipe = new DrillPipe(keepJSON, paths.toArray(new String[0]), col);
+		//DrillPipe pipe = new DrillPipe(keepJSON, paths.toArray(new String[0]), col);
+        
+        //get the catalog filename from index filename
+        String catalog = indexFile.substring(0, indexFile.indexOf(".")+1) + ".tsv.bgz";
+        
+        LookupPipe pipe = new LookupPipe(indexFile, catalog);
 		
 		mPipeline.execute(pipe);		
 	}
