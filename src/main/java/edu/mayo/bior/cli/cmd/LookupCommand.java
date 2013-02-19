1c7bb361e4355dbd7aec36eb5f66a7bc36c14ee8
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-19 22:42:46 +0000

    included jsonpath, column, index file options

--- src/main/java/edu/mayo/bior/cli/cmd/LookupCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/LookupCommand.java
@@ -12,6 +12,9 @@ public class LookupCommand implements CommandPlugin {
 
 	private static final char OPTION_DRILL_COLUMN = 'c';
 	private static final char INDEX_FILE = 'd';
+	// JSON path to extract key (if column is specified, the json in that column is used.
+	// If not, then the last column is used)
+	private static final char OPTION_JSON_PATH = 'p';
 		
 	private UnixStreamPipeline mPipeline = new UnixStreamPipeline();
 	
@@ -19,22 +22,26 @@ public class LookupCommand implements CommandPlugin {
 	}
 
 	public void execute(CommandLine line) throws Exception {
-		String indexFile = "";
+		String indexFilePath = "";
+		
+		String catalogPath = line.getArgs()[0];
+		
+		// JSON may be null if parameter not specified
+		// (NOTE: It is NOT required - if not specified, then the entire column will be used as Id)
+		String jsonPath = line.getOptionValue(OPTION_JSON_PATH);
+		
 		if (line.hasOption(INDEX_FILE)) {
-			indexFile  = line.getOptionValue(INDEX_FILE);
+			indexFilePath  = line.getOptionValue(INDEX_FILE);
+		} else {
+			//find the index file based on catalog-name??
 		}
                 
-        Integer col = -1;
+        Integer column = -1;
         if (line.hasOption(OPTION_DRILL_COLUMN)) {
-        	col = new Integer(line.getOptionValue(OPTION_DRILL_COLUMN));
+        	column = new Integer(line.getOptionValue(OPTION_DRILL_COLUMN));
         }
-	
-		//DrillPipe pipe = new DrillPipe(keepJSON, paths.toArray(new String[0]), col);
-        
-        //get the catalog filename from index filename
-        String catalog = indexFile.substring(0, indexFile.indexOf(".")) + ".tsv.bgz";
-        
-        LookupPipe pipe = new LookupPipe(indexFile, catalog);
+	    
+        LookupPipe pipe = new LookupPipe(catalogPath, indexFilePath, jsonPath, column);
 		
 		mPipeline.execute(pipe);		
 	}
