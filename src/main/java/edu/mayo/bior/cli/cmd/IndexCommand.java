902240060d27bd29d8093d5dffadde1eb05c04f8
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-20 04:45:51 +0000

    Removed column flag Changed index path to a flag (vs arg)

--- src/main/java/edu/mayo/bior/cli/cmd/IndexCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/IndexCommand.java
@@ -1,6 +1,7 @@
 package edu.mayo.bior.cli.cmd;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
@@ -12,8 +13,8 @@ import edu.mayo.pipes.util.index.IndexDatabaseCreator;
 /** Create an index H2 database from a catalog bgzip file */
 public class IndexCommand implements CommandPlugin {
 
-	// Column to find key
-	private static final char OPTION_COLUMN_CONTAINING_KEY = 'c';
+	// Index database path to write
+	private static final char OPTION_INDEX_PATH = 'x';
 
 	// JSON path to extract key (if column is specified, the json in that column is used.
 	// If not, then the last column is used)
@@ -22,28 +23,39 @@ public class IndexCommand implements CommandPlugin {
 	public void init(Properties props) throws Exception {
 	}
 
-	// Example (selects key from column 4 with JsonPath "HGNC" and the key is an integer):
-	//   bior_index  /data/catalogs/NCBIGene/GRCh37_p10/genes.tsv.bgz  /data/catalogs/NCBIGene/GRCh37_p10/index/genes.HGNC.idx.h2.db  -c 4  -p HGNC  -i true
+	// Example
+	//   bior_index  /data/catalogs/NCBIGene/GRCh37_p10/genes.tsv.bgz  -p HGNC  -x /data/catalogs/NCBIGene/GRCh37_p10/index/genes.HGNC.idx.h2.db
 	public void execute(CommandLine line) throws Exception {
 		String bgzipPath 	= line.getArgs()[0];
-		String h2DbOutPath 	= line.getArgs()[1];
-
-		// JSON may be null if parameter not specified
-		// (NOTE: It is NOT required - if not specified, then the entire column will be used as Id)
+		
+		// JsonPath is required
 		String jsonPath = line.getOptionValue(OPTION_JSON_PATH);
-                
-		// If no column is specified then use the last column
-		int col = -1;
-		if (line.hasOption(OPTION_COLUMN_CONTAINING_KEY)) {
-			col = Integer.parseInt(line.getOptionValue(OPTION_COLUMN_CONTAINING_KEY));
-		}
 
-		File indexOutFile = new File(h2DbOutPath);
+		// Index path is optional - use it if supplied, else create it from the bgzip catalog path
+		String indexDbPathOut = line.getOptionValue(OPTION_INDEX_PATH);
+		if(indexDbPathOut == null)
+			indexDbPathOut = buildIndexPath(bgzipPath, jsonPath);
+
+		File indexOutFile = new File(indexDbPathOut);
 		if( ! indexOutFile.getParentFile().exists())
 			indexOutFile.getParentFile().mkdirs();
 		
 		IndexDatabaseCreator indexer = new IndexDatabaseCreator();
-		indexer.buildIndexH2(bgzipPath, col, jsonPath, h2DbOutPath);
+		indexer.buildIndexH2(bgzipPath, -1, jsonPath, indexDbPathOut);
+	}
+
+	private String buildIndexPath(String bgzipPath, String jsonPath) throws IOException {
+		File bgzipFile = new File(bgzipPath);
+		
+		// Get the catalog prefix up to the first dot
+		String bgzipPrefix = bgzipFile.getName();
+		int idxFirstDot = bgzipPrefix.indexOf(".");
+		if(idxFirstDot != -1)
+			bgzipPrefix = bgzipPrefix.substring(0, idxFirstDot);
+		
+		File bgzipParentDir = bgzipFile.getParentFile();
+		String fullIndexPath = bgzipParentDir.getCanonicalPath() + "/index/" + bgzipPrefix + "." + jsonPath + ".idx.h2.db";
+		return fullIndexPath;
 	}
 
 }
