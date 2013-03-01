efa70998868e719567009df42ef837c47087e27f
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-03-01 15:27:53 +0000

    changes to options

--- src/main/java/edu/mayo/bior/cli/cmd/IndexCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/IndexCommand.java
@@ -1,14 +1,14 @@
 package edu.mayo.bior.cli.cmd;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 
 import edu.mayo.bior.cli.CommandPlugin;
-import edu.mayo.bior.pipeline.UnixStreamPipeline;
+import edu.mayo.bior.cli.InvalidOptionArgValueException;
+import edu.mayo.pipes.JSON.lookup.lookupUtils.IndexUtils;
 import edu.mayo.pipes.util.index.IndexDatabaseCreator;
 
 /** Create an index H2 database from a catalog bgzip file */
@@ -17,49 +17,39 @@ public class IndexCommand implements CommandPlugin {
 	private static final char OPTION_CATALOG = 'd';
 	
 	// Index database path to write
-	private static final char OPTION_INDEX_PATH = 'x';
+	private static final char OPTION_INDEX_PATH = 'i';
 
 	// JSON path to extract key (if column is specified, the json in that column is used.
 	// If not, then the last column is used)
-	private static final char OPTION_JSON_PATH = 'k';
+	private static final char OPTION_KEY = 'p';
 	
 	public void init(Properties props) throws Exception {
 	}
 
 	// Example
-	//   bior_index  /data/catalogs/NCBIGene/GRCh37_p10/genes.tsv.bgz  -p HGNC  -x /data/catalogs/NCBIGene/GRCh37_p10/index/genes.HGNC.idx.h2.db
+	//   bior_index  -d /data/catalogs/NCBIGene/GRCh37_p10/genes.tsv.bgz  -p HGNC  -i /data/catalogs/NCBIGene/GRCh37_p10/index/genes.HGNC.idx.h2.db
 	public void execute(CommandLine line, Options opts) throws Exception {
-		// Catalog path is required
-		String bgzipPath 	= line.getOptionValue(OPTION_CATALOG);
-		
-		// JsonPath is required
-		String jsonPath = line.getOptionValue(OPTION_JSON_PATH);
+		// Catalog path and key are required
+		String bgzipPath = line.getOptionValue(OPTION_CATALOG);
+		String key 		 = line.getOptionValue(OPTION_KEY);
 
+		// Throw error if catalog does not exist
+		if( ! new File(bgzipPath).exists() ) {
+			throw new InvalidOptionArgValueException(
+					opts.getOption(OPTION_CATALOG + ""),
+					bgzipPath,
+					"Catalog file could not be found: " + bgzipPath
+					);
+		}
+		
 		// Index path is optional - use it if supplied, else create it from the bgzip catalog path
 		String indexDbPathOut = line.getOptionValue(OPTION_INDEX_PATH);
 		if(indexDbPathOut == null)
-			indexDbPathOut = buildIndexPath(bgzipPath, jsonPath);
-
-		File indexOutFile = new File(indexDbPathOut);
-		if( ! indexOutFile.getParentFile().exists())
-			indexOutFile.getParentFile().mkdirs();
+			indexDbPathOut = IndexUtils.buildIndexPath(bgzipPath, key);
+		
+		IndexUtils.createParentDirectories(indexDbPathOut);
 		
 		IndexDatabaseCreator indexer = new IndexDatabaseCreator();
-		indexer.buildIndexH2(bgzipPath, -1, jsonPath, indexDbPathOut);
+		indexer.buildIndexH2(bgzipPath, -1, key, indexDbPathOut);
 	}
-
-	private String buildIndexPath(String bgzipPath, String jsonPath) throws IOException {
-		File bgzipFile = new File(bgzipPath);
-		
-		// Get the catalog prefix up to the first dot
-		String bgzipPrefix = bgzipFile.getName();
-		int idxFirstDot = bgzipPrefix.indexOf(".");
-		if(idxFirstDot != -1)
-			bgzipPrefix = bgzipPrefix.substring(0, idxFirstDot);
-		
-		File bgzipParentDir = bgzipFile.getParentFile();
-		String fullIndexPath = bgzipParentDir.getCanonicalPath() + "/index/" + bgzipPrefix + "." + jsonPath + ".idx.h2.db";
-		return fullIndexPath;
-	}
-
 }
