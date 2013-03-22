8875c5ffdde85bc1ff7ac48083de35afd6d09bdd
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-03-22 20:34:37 +0000

    adding a new pipe to take the output from snpeff and turn it into a history

---
+++ src/main/java/edu/mayo/bior/pipeline/SNPEff/VCFProgram2HistoryPipe.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mayo.bior.pipeline.SNPEff;
+
+import com.tinkerpop.pipes.AbstractPipe;
+import edu.mayo.pipes.history.ColumnMetaData;
+import edu.mayo.pipes.history.History;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author m102417
+ */
+public class VCFProgram2HistoryPipe extends AbstractPipe<String, History> {
+
+    public History histAppend(String[] tokens){
+        History h = new History();
+        for(int i=0; i<tokens.length; i++){
+            h.add(tokens[i]);
+        }
+        return h;
+    }
+
+    @Override
+    protected History processNextStart() throws NoSuchElementException {
+        StringBuilder sb = new StringBuilder();
+        String line = this.starts.next();
+        History h = null;
+        if(line.startsWith("#")){
+            //there is header intermingled with data...
+            if(line.contains("\n")){
+                String[] split = line.split("\n");
+                for(int i=0; i<split.length; i++){
+                    //remove the header and get the data
+                    if(split[i].startsWith("#")){
+                        ;//throw it away
+                    }else{
+                        h = histAppend(split[i].split("\t"));//this should just happen once
+                    }
+                }
+            }
+        }else{//no newlines... create the history out of the line directly
+                h = histAppend(line.split("\t"));
+        }
+        if(h == null){
+            return processNextStart();
+        }
+
+        return h;
+    }
+    
+}
