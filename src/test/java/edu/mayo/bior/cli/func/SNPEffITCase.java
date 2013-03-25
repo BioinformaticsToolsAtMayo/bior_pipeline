2467818144d05a633219498945ea0c83bd9d4eef
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-03-25 06:11:49 +0000

    Added testSNPEFF test case.

--- src/test/java/edu/mayo/bior/cli/func/SNPEffITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/SNPEffITCase.java
@@ -4,40 +4,36 @@
  */
 package edu.mayo.bior.cli.func;
 
-import com.tinkerpop.pipes.Pipe;
-import com.tinkerpop.pipes.PipeFunction;
-import com.tinkerpop.pipes.transform.TransformFunctionPipe;
-import com.tinkerpop.pipes.util.Pipeline;
-import edu.mayo.bior.pipeline.SNPEff.SNPEffPreProcessPipe;
-import edu.mayo.bior.pipeline.SNPEff.VCFProgram2HistoryPipe;
-import edu.mayo.exec.Command;
-import edu.mayo.exec.UnixStreamCommand;
-import edu.mayo.pipes.DrainPipe;
-import edu.mayo.pipes.ExecPipe;
-import edu.mayo.pipes.HeaderPipe;
-import edu.mayo.pipes.MergePipe;
-import edu.mayo.pipes.PrintPipe;
-import edu.mayo.pipes.SplitPipe;
-import edu.mayo.pipes.UNIX.CatPipe;
-import edu.mayo.pipes.UNIX.GrepEPipe;
-import edu.mayo.pipes.WritePipe;
-import edu.mayo.pipes.aggregators.AggregatorPipe;
-import edu.mayo.pipes.history.HCutPipe;
-import edu.mayo.pipes.history.History;
-import edu.mayo.pipes.history.HistoryInPipe;
+import static org.junit.Assert.assertEquals;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.TimeoutException;
+
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
+import com.tinkerpop.pipes.PipeFunction;
+import com.tinkerpop.pipes.util.Pipeline;
+
+import edu.mayo.bior.pipeline.SNPEff.SNPEffPreProcessPipe;
+import edu.mayo.bior.pipeline.SNPEff.VCFProgram2HistoryPipe;
+import edu.mayo.exec.Command;
+import edu.mayo.exec.UnixStreamCommand;
+import edu.mayo.pipes.ExecPipe;
+import edu.mayo.pipes.MergePipe;
+import edu.mayo.pipes.PrintPipe;
+import edu.mayo.pipes.UNIX.CatPipe;
+import edu.mayo.pipes.history.HistoryInPipe;
 
 /**
  *
@@ -136,44 +132,135 @@ public class SNPEffITCase {
         }
         
         
-        @Test
-        public void testExecSNPEffWOPipe() throws IOException, InterruptedException{
-            System.out.println("Test the raw output of a run on SNPEff");
-            
-            UnixStreamCommand cmd = new UnixStreamCommand(command, NO_CUSTOM_ENV, true,  UnixStreamCommand.StdoutBufferingMode.LINE_BUFFERED, 0);
-            cmd.launch();
-            
-            BufferedReader br = new BufferedReader(new FileReader("src/test/resources/tools/snpeff/snpEffOutput205.vcf"));
-            BufferedReader br2 = new BufferedReader(new FileReader("src/test/resources/tools/treat/treatInput.vcf"));
-            String line;
-            List<String> output;
-            int count = 0;
-            br.readLine();
-            br.readLine();
-            br.readLine();
-            br2.readLine();
-            br2.readLine();
-            while((line = br.readLine()) != null){
-                count++;
-                System.out.println(line);
-                String input = br2.readLine();
-                if(!input.startsWith("#")){
-                    if(count==1){
-                        cmd.send(Arrays.asList("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n" + input));
-                    }else{
-                        cmd.send(Arrays.asList(input));
-                        output = cmd.receive();
-                        System.out.println(output.get(0));
-                        if(count >2){
-                            assertEquals(line,output.get(0));
-                        }
-                    }
-                }
-            }
-
-        }
+        //@Test
+//        public void testExecSNPEffWOPipe() throws IOException, InterruptedException{
+//            System.out.println("Test the raw output of a run on SNPEff");
+//            
+//            UnixStreamCommand cmd = new UnixStreamCommand(command, NO_CUSTOM_ENV, true,  UnixStreamCommand.StdoutBufferingMode.LINE_BUFFERED, 0);
+//            cmd.launch();
+//            
+//            BufferedReader br = new BufferedReader(new FileReader("src/test/resources/tools/snpeff/snpEffOutput205.vcf"));
+//            BufferedReader br2 = new BufferedReader(new FileReader("src/test/resources/tools/treat/treatInput.vcf"));
+//            String line;
+//            List<String> output;
+//            int count = 0;
+//            br.readLine();
+//            br.readLine();
+//            br.readLine();
+//            br2.readLine();
+//            br2.readLine();
+//            while((line = br.readLine()) != null){
+//                count++;
+//                System.out.println(line);
+//                String input = br2.readLine();
+//                if(!input.startsWith("#")){
+//                    if(count==1){
+//                        cmd.send(Arrays.asList("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n" + input));
+//                    }else{
+//                        cmd.send(Arrays.asList(input));
+//                        output = cmd.receive();
+//                        System.out.println(output.get(0));
+//                        if(count >2){
+//                            assertEquals(line,output.get(0));
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
         
+        @Test
+        public void testSNPEFF() throws IOException, InterruptedException, BrokenBarrierException, TimeoutException{
 
+            final String[] snpeffCmdArray =
+            	{
+            		"java", 
+                    "-Xmx4g", 
+                     "-jar", 
+                     "/Applications/snpEff_2_0_5/snpEff.jar",
+                     "eff",
+                     "-c",
+                     "/Applications/snpEff_2_0_5/snpEff.config",
+                     "-v",
+                     "GRCh37.64",
+                     "-o",
+                     "vcf",
+                     "-noLog",
+                     "-noStats"
+                };
+            UnixStreamCommand snpeff = new UnixStreamCommand(snpeffCmdArray, NO_CUSTOM_ENV, true, true);        	
+        	
+        	File inputFile    = new File("src/test/resources/tools/treat/treatInput.vcf");            
+            File outputFile   = new File("/tmp/snpEffOutput.vcf");
+            File expectedFile = new File("src/test/resources/tools/snpeff/snpEffOutput205.vcf");
+
+            // make sure we start with a clean output file
+            if (outputFile.exists()) {
+            	outputFile.delete();
+            	outputFile.createNewFile();
+            }            
+
+            BufferedReader br = new BufferedReader(new FileReader(inputFile));
+            PrintWriter pw = new PrintWriter(new FileWriter(outputFile));            
+            
+            // launch snpeff java process
+            snpeff.launch();
+            
+            // send VCF header, this is required
+            snpeff.send("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO");
+
+            boolean outputHeaderProcessed = false;
+            
+            String line = br.readLine();
+            while( line != null ) {
+
+            	// only send VCF data lines to snpeff
+                if(!line.startsWith("#")){
+                	
+                	// send data line to snpeff
+                    snpeff.send(line);
+
+                    // receive data line from snpeff
+                    String outputLine = snpeff.receive();
+                    
+                    // handle header outputted from SNPEFF
+                    while (outputLine.startsWith("#")) {
+                    	pw.println(outputLine);
+                        pw.flush();
+                        outputLine = snpeff.receive();
+                    }
+                    
+                	pw.println(outputLine);
+                    pw.flush();
+                }
+                
+                line = br.readLine();
+            }            
+            // close stream to output file
+            pw.close();
+            
+            // tell SNPEFF we're done
+            snpeff.terminate();
+            
+            // perform diff between output file and expected file
+            String[] diffArray = new String[] 
+            	{
+            		"diff",
+            		expectedFile.getAbsolutePath(),
+            		outputFile.getAbsolutePath()
+            	};
+            Command diff = new Command(diffArray, NO_CUSTOM_ENV, true);
+            diff.execute();
+            System.out.println(
+            	String.format(
+            		"Output from running: diff %s %s",
+            		expectedFile.getAbsolutePath(),
+            		outputFile.getAbsolutePath()            		
+            	)
+            );            
+            // show diff output in console
+            System.out.println(diff.getStdout());
+        }
     
 //        @Test
 //        public void testTreat(){
@@ -194,38 +281,38 @@ public class SNPEffITCase {
 //            }
 //        }
         
-        public class Pad implements PipeFunction<ArrayList<String>,ArrayList<String>> {
-
-            @Override
-            public ArrayList<String> compute(ArrayList<String> a) {
-                ArrayList<String> ret = new ArrayList<String>();
-                ArrayList<Integer> keep = new ArrayList<Integer>();  
-                keep.add(1);
-                keep.add(2);
-                keep.add(3);
-                keep.add(18);
-                keep.add(19);
-//                keep.add(58);
-//                keep.add(59);
-//                keep.add(60);
-//                keep.add(61);
-//                keep.add(62);
-//                keep.add(63);
-//                keep.add(64);
-//                keep.add(65);
-//                keep.add(66);
-//                keep.add(67);
-//                keep.add(68);
-                for(int i=0; i<a.size(); i++){
-                    if(keep.contains(new Integer(i))){
-                        ret.add(a.get(i));
-                    }
-                }
-                ret.add(5, ".");
-                ret.add(6, ".");
-                ret.add(7, ".");
-                //System.out.println(a.size());
-                return ret;
-            }
-        }
+//        public class Pad implements PipeFunction<ArrayList<String>,ArrayList<String>> {
+//
+//            @Override
+//            public ArrayList<String> compute(ArrayList<String> a) {
+//                ArrayList<String> ret = new ArrayList<String>();
+//                ArrayList<Integer> keep = new ArrayList<Integer>();  
+//                keep.add(1);
+//                keep.add(2);
+//                keep.add(3);
+//                keep.add(18);
+//                keep.add(19);
+////                keep.add(58);
+////                keep.add(59);
+////                keep.add(60);
+////                keep.add(61);
+////                keep.add(62);
+////                keep.add(63);
+////                keep.add(64);
+////                keep.add(65);
+////                keep.add(66);
+////                keep.add(67);
+////                keep.add(68);
+//                for(int i=0; i<a.size(); i++){
+//                    if(keep.contains(new Integer(i))){
+//                        ret.add(a.get(i));
+//                    }
+//                }
+//                ret.add(5, ".");
+//                ret.add(6, ".");
+//                ret.add(7, ".");
+//                //System.out.println(a.size());
+//                return ret;
+//            }
+//        }
 }
