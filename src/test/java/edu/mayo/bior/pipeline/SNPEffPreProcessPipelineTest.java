ff3aeffe8ff1e9bfea353e8bb445fac55a5862a7
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-03-27 20:15:34 +0000

    SNPEff pre-pipeline test (cleans up data to send to SnpEff)

---
+++ src/test/java/edu/mayo/bior/pipeline/SNPEffPreProcessPipelineTest.java
@@ -0,0 +1,32 @@
+package edu.mayo.bior.pipeline;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.tinkerpop.pipes.util.Pipeline;
+
+import edu.mayo.bior.pipeline.SNPEff.SNPEffPreProcessPipe;
+import edu.mayo.pipes.UNIX.CatPipe;
+import edu.mayo.pipes.history.HistoryInPipe;
+import edu.mayo.pipes.util.test.FileCompareUtils;
+import edu.mayo.pipes.util.test.PipeTestUtils;
+
+public class SNPEffPreProcessPipelineTest {
+
+	@Test
+	public void verifyOutput() throws IOException {
+		Pipeline pipe = new Pipeline(
+				new CatPipe(),
+				new HistoryInPipe(),
+				new SNPEffPreProcessPipe()
+				);
+		pipe.setStarts(Arrays.asList("src/test/resources/tools/snpeff/snpEff.preProcess.input.1.vcf"));
+		ArrayList<String> actual = PipeTestUtils.pipeOutputToStrings2(pipe);
+		ArrayList<String> expected = (ArrayList<String>) FileCompareUtils.loadFile(
+				"src/test/resources/tools/snpeff/snpEff.preProcess.expected.1.vcf");
+		PipeTestUtils.assertListsEqual(expected, actual);
+	}
+}
