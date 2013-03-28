0d6846f7abcf7d98db2346cb2bc799e51eeabd4b
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-03-28 18:57:59 +0000

    working version of snpeff

--- src/test/java/edu/mayo/bior/pipeline/SNPEff/SNPEFFMergeTest.java
+++ src/test/java/edu/mayo/bior/pipeline/SNPEff/SNPEFFMergeTest.java
@@ -4,15 +4,18 @@
  */
 package edu.mayo.bior.pipeline.SNPEff;
 
+import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.transform.IdentityPipe;
 import com.tinkerpop.pipes.util.Pipeline;
 import edu.mayo.pipes.MergePipe;
 import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.ReplaceAllPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
+import edu.mayo.pipes.UNIX.GrepEPipe;
 import edu.mayo.pipes.history.HCutPipe;
 import edu.mayo.pipes.history.History;
 import edu.mayo.pipes.history.HistoryInPipe;
+import edu.mayo.pipes.history.HistoryOutPipe;
 import edu.mayo.pipes.meta.BridgeOverPipe;
 import java.util.Arrays;
 import org.junit.After;
@@ -50,7 +53,6 @@ public class SNPEFFMergeTest {
         @Test
         public void testBridge(){
             System.out.println("testBridge");
-            String appendMe = "D";
             int[] cols = {1};
             Pipeline superviseMe = new Pipeline(
                     new MergePipe("\t"),
@@ -89,4 +91,52 @@ public class SNPEFFMergeTest {
                 
             }
         }
+        @Test
+        public void testBridge2(){
+            System.out.println("Test Bridge with Post-Process Pipe");
+            SNPEffPostProcessPipeline ppp = new SNPEffPostProcessPipeline(true);           
+            Pipe post = ppp.getSNPEffTransformPipe(true);
+            Pipeline superviseMe = new Pipeline(
+                    new SNPEffPreProcessPipe(8),//history-string //when not running this test, you will not want col 8, so use 7 here!
+                    new VCFProgram2HistoryPipe(),//string-history
+                    post //history-history
+                    );
+            BridgeOverPipe bridge = new BridgeOverPipe(superviseMe, new SNPEFFMerge());
+            Pipeline p = new Pipeline(
+                    new CatPipe(),//filename-string
+                    new GrepEPipe("^#"),//string-string
+                    new HistoryInPipe(),//string-history
+                    //new PrintPipe(),
+                    bridge, //the BridgeOverPipe
+                    new IdentityPipe()
+                    //new PrintPipe()
+            );
+            p.setStarts(Arrays.asList("src/test/resources/tools/snpeff/snpEffOutput205.vcf"));
+            for(int i=0; p.hasNext(); i++){
+                History h = (History) p.next();
+                //p.next();
+                if(i==0){
+                    assertEquals("chr1", h.get(0));
+                    assertEquals("949608", h.get(1));
+                    assertEquals("rs1921", h.get(2));
+                    assertEquals("G", h.get(3));
+                    assertEquals("A", h.get(4));
+                    assertEquals("0.0", h.get(5));
+                    assertEquals(".", h.get(6));                  
+                    assertEquals("EFF=NON_SYNONYMOUS_CODING(MODERATE|MISSENSE|aGc/aAc|S83N|ISG15|protein_coding|CODING|ENST00000379389|exon_1_949364_949920),UPSTREAM(MODIFIER||||RP11-54O7.11|antisense|NON_CODING|ENST00000458555|)", h.get(7));
+                    assertEquals("{\"Effect\":\"NON_SYNONYMOUS_CODING\",\"Effect_impact\":\"MODERATE\",\"Functional_class\":\"MISSENSE\",\"Codon_change\":\"aGc/aAc\",\"Amino_acid_change\":\"S83N\",\"Gene_name\":\"ISG15\",\"Gene_bioType\":\"protein_coding\",\"Coding\":\"CODING\",\"Transcript\":\"ENST00000379389\",\"Exon\":\"exon_1_949364_949920\"}", h.get(8));                   
+//chr1, 949608, rs1921, G, A, 0.0, ., EFF=NON_SYNONYMOUS_CODING(MODERATE|MISSENSE|aGc/aAc|S83N|ISG15|protein_coding|CODING|ENST00000379389|exon_1_949364_949920),UPSTREAM(MODIFIER||||RP11-54O7.11|antisense|NON_CODING|ENST00000458555|), foo]
+
+                }
+                if(i==1){
+
+                }
+                //if(i==4)break;
+                  
+                
+            }
+        }
+
+
+
 }
