8367ad5c80d69ea73dc555bcee54596c3884aa88
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-04-15 19:50:12 +0000

    Added cases from VEPEXETest (which were functional tests)

--- src/test/java/edu/mayo/bior/cli/func/remoteexec/VEPITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/remoteexec/VEPITCase.java
@@ -17,18 +17,25 @@ import org.junit.Test;
 
 import com.google.gson.Gson;
 import com.jayway.jsonpath.JsonPath;
+import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.transform.IdentityPipe;
+import com.tinkerpop.pipes.transform.TransformFunctionPipe;
 import com.tinkerpop.pipes.util.Pipeline;
 
 import edu.mayo.bior.cli.func.BaseFunctionalTest;
 import edu.mayo.bior.cli.func.CommandOutput;
 import edu.mayo.bior.cli.func.remoteexec.helpers.RemoteFunctionalTest;
+import edu.mayo.bior.pipeline.VCFProgramPipes.VCFProgram2HistoryPipe;
 import edu.mayo.bior.pipeline.VEP.VEPEXE;
+import edu.mayo.bior.pipeline.VEP.VEPPipeline;
+import edu.mayo.bior.pipeline.VEP.VEPPostProcessingPipeline;
 import edu.mayo.exec.AbnormalExitException;
 import edu.mayo.pipes.MergePipe;
 import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
 import edu.mayo.pipes.history.HistoryInPipe;
+import edu.mayo.pipes.history.HistoryOutPipe;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
@@ -50,27 +57,27 @@ import java.util.concurrent.TimeoutException;
  */
 public class VEPITCase extends RemoteFunctionalTest {
 
-//	@BeforeClass
-//	public static void beforeAllVep() {
-//		System.out.println("BeforeAll - VEP");
-//	}
-//	
-//	@AfterClass
-//	public static void afterAllVep() {
-//		System.out.println("AfterAll - VEP");
-//	}
-//	
-//	@Before
-//	public void beforeEachVep() {
-//		System.out.println("BeforeEach - VEP");
-//	}
-//
-//	@After
-//	public void afterEachVep() {
-//		System.out.println("AfterEach - VEP");
-//	}
-    
-    	/**
+	//	@BeforeClass
+	//	public static void beforeAllVep() {
+	//		System.out.println("BeforeAll - VEP");
+	//	}
+	//	
+	//	@AfterClass
+	//	public static void afterAllVep() {
+	//		System.out.println("AfterAll - VEP");
+	//	}
+	//	
+	//	@Before
+	//	public void beforeEachVep() {
+	//		System.out.println("BeforeEach - VEP");
+	//	}
+	//
+	//	@After
+	//	public void afterEachVep() {
+	//		System.out.println("AfterEach - VEP");
+	//	}
+
+	/**
 	 * note: if you want to dig deep and debug this code, you probably want to set your log4j properties to:
 	 * ##active
            ## log4j configuration used during build and unit tests
@@ -85,10 +92,10 @@ public class VEPITCase extends RemoteFunctionalTest {
 	 * 
 	 * @throws IOException
 	 * @throws InterruptedException 
-    	 * @throws AbnormalExitException 
+	 * @throws AbnormalExitException 
 	 */
 	//@Test
-	public void testExecSNPEffPipe() throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
+	public void testExecVepPipe() throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
 		System.out.println("Test the raw output of a run on SNPEff versus the expected output (w/o header)");
 		VEPEXE vep = new VEPEXE();
 		//Pipe t = new TransformFunctionPipe(snp);
@@ -103,25 +110,125 @@ public class VEPITCase extends RemoteFunctionalTest {
 		p.setStarts(Arrays.asList("src/test/resources/tools/vep/example.vcf"));
 		//expected results
 		BufferedReader br = new BufferedReader(new FileReader("src/test/resources/tools/vep/example.vcf.vep"));
-                String expected = "";
-                while(expected != null){
-                    expected = br.readLine();
-                    System.out.println(expected);
-                }
-//		for(int i=1;p.hasNext();i++){
-//			//System.out.println(i);
-//			String o = (String) p.next();       //result from the pipeline
-//			String res = (String) br.readLine();//result is the output file
-//			while(res.startsWith("#")){//if it is a header line, skip it
-//				res = (String) br.readLine();
-//			}
-//			//System.out.println("CALCULATED: " + o);
-//			//System.out.println("OUTPUT    : " + res);
-//			assertEquals(res,o);
-//			//if(i==10) break;
-//		}
+		String expected = "";
+		while(expected != null){
+			expected = br.readLine();
+			System.out.println(expected);
+		}
+		//		for(int i=1;p.hasNext();i++){
+		//			//System.out.println(i);
+		//			String o = (String) p.next();       //result from the pipeline
+		//			String res = (String) br.readLine();//result is the output file
+		//			while(res.startsWith("#")){//if it is a header line, skip it
+		//				res = (String) br.readLine();
+		//			}
+		//			//System.out.println("CALCULATED: " + o);
+		//			//System.out.println("OUTPUT    : " + res);
+		//			assertEquals(res,o);
+		//			//if(i==10) break;
+		//		}
 	}
-	
+
+
+	/**
+	 * This pipeline will clean the input before passing it to vep, then stitch it back together
+	 */
+	@Test
+	public void testVEPBridgePipeline() throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
+		Pipe input = new Pipeline(new CatPipe(), new HistoryInPipe());
+		//Pipeline p = new VEPPipeline(VEPEXE.getVEPMac("1"), input, new PrintPipe(), true);
+		Pipeline p = new VEPPipeline(VEPEXE.getVEPCommand("1"), input, new PrintPipe(), false);
+		p.setStarts(Arrays.asList("src/test/resources/tools/vep/example.vcf"));
+		while(p.hasNext()){
+			p.next();
+		}
+	}
+
+	//moveMe - a functional test for the new VEP Pipeline
+	//This is a little test to make sure everything is working prior to integrating the bridgeover functions
+	//@Test
+	public void testVEPPipeline() throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
+		System.out.println("testVEPPipeline");
+		String[] vepHeader = new String[1];
+		vepHeader[0] = "##INFO=<ID=CSQ,Number=.,Type=String,Description=\"Consequence type as predicted by VEP. Format: Allele|Gene|Feature|Feature_type|Consequence|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|SIFT|PolyPhen|CELL_TYPE\">";
+		VEPEXE vep = new VEPEXE(VEPEXE.getVEPCommand("1"));
+		Pipe exe = new TransformFunctionPipe(vep);
+		VEPPostProcessingPipeline ppp = new VEPPostProcessingPipeline();
+		//Pipe post = ppp.getWorstCasePipeline(new IdentityPipe(), new IdentityPipe(), false);
+		Pipe post = ppp.getCartesianProductPipeline(new IdentityPipe(), new IdentityPipe(),false);
+
+		Pipeline p = new Pipeline(
+				new CatPipe(),
+				new HistoryInPipe(),         //get rid of the header
+				new MergePipe("\t"),
+				exe,
+				new VCFProgram2HistoryPipe(vepHeader),   
+				post,
+				new HistoryOutPipe(),
+				new PrintPipe()
+				);
+		p.setStarts(Arrays.asList("src/test/resources/tools/vep/example.vcf"));
+		for(int i=0; p.hasNext(); i++){
+			p.next();
+		}
+		vep.terminate();
+	}
+
+	//moveMe -- a functional test that we need to move to the vepitcase
+	/**
+	 * note: if you want to dig deep and debug this code, you probably want to set your log4j properties to:
+	 * ##active
+           ## log4j configuration used during build and unit tests
+           #log4j.rootLogger=DEBUG, console
+           #log4j.threshhold=ALL
+
+           ## console appender logs to STDOUT
+           #log4j.appender.console=org.apache.log4j.ConsoleAppender
+           ##log4j.appender.console.layout=org.apache.log4j.PatternLayout
+           #log4j.appender.console.layout.ConversionPattern=%d [%t] %-5p %c - %m%n
+	 * 
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 * @throws AbnormalExitException 
+	 */
+	//@Test
+	// TODO: Disabled until Dan can refactor
+	public void testExecSNPEffPipe() throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
+		System.out.println("Test the raw output of a run on SNPEff versus the expected output (w/o header)");
+		VEPEXE vep = new VEPEXE(VEPEXE.getVEPCommand("1"));
+		Pipe t = new TransformFunctionPipe(vep);
+		Pipeline p = new Pipeline(
+				new CatPipe(),               //raw file
+				new HistoryInPipe(),         //get rid of the header
+				new MergePipe("\t"),
+				t,
+				//new PrintPipe()
+				new IdentityPipe()
+				);
+		p.setStarts(Arrays.asList("src/test/resources/tools/vep/example.vcf"));
+		//expected results
+		BufferedReader br = new BufferedReader(new FileReader("src/test/resources/tools/vep/example.vcf.vep.correct"));
+
+		String expected = "";
+		String res = "";
+
+		//remove the header
+		br.readLine();
+		br.readLine();
+		br.readLine();
+
+		while(expected != null){
+			expected = br.readLine();
+			if(expected == null) break;
+			System.out.println("Expected: " + expected);
+			res = (String) p.next();
+			System.out.println("Result: " + res);
+			assertEquals(expected, res);
+		}
+
+	}
+
 	//@Test
 	public void testCatalogLocationDbSnp() {
 		System.out.println("VEPITCase.testCatalogLocationDbSnp(): Verify that the dbSNP catalog file exists and is > 2GB...");
@@ -131,11 +238,11 @@ public class VEPITCase extends RemoteFunctionalTest {
 		Assert.assertTrue(dbSnp.length() > twoGB);
 		System.out.println("  Verified.");
 	}
-	
 
-	
-    //@Test
-    public void test() throws IOException, InterruptedException {
+
+
+	//@Test
+	public void test() throws IOException, InterruptedException {
 		System.out.println("VEPITCase.test(): testing sample VEP vcf file");
 		// NOTE:  This test case should only run on biordev - where it can run VEP
 		String stdin = loadFile(new File("src/test/resources/tools/vep/vepsample.vcf"));
@@ -163,9 +270,9 @@ public class VEPITCase extends RemoteFunctionalTest {
 
 	}
 
-	
+
 	@Test
 	public void test2() {
-		
+
 	}
 }
