0097c626787cf83418a7a605d9f2b5e4f9d75f22
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-04-23 19:16:54 +0000

    Test user-specified parms

--- src/test/java/edu/mayo/bior/pipeline/VEP/VEPEXETest.java
+++ src/test/java/edu/mayo/bior/pipeline/VEP/VEPEXETest.java
@@ -14,17 +14,39 @@ public class VEPEXETest {
     /** Test of getVEPCommand method, of class VEPEXE. */
     @Test
     public void testGetVEPCommand() throws Exception {
-        System.out.println("getVEPCommand");
-        String bufferSize = "20";
+        System.out.println("VEPEXETest.testGetVEPCommand()");
         String[] actual = VEPEXE.getVEPCommand(null);
         
         String[] expected = { 
         	"", "", "-i", "/dev/stdin", "-o",
         	"STDOUT", "-dir", "", "-vcf", "--hgnc", 
         	"-polyphen", "b", "-sift", "b", "--offline",
-        	"--buffer_size", "20"
+        	"--buffer_size", "1"
         };
         
+        assertVepCmd(expected, actual);
+    }
+    
+    /** Test of getVEPCommand method, of class VEPEXE. */
+    @Test
+    public void testWithUserInputs() throws Exception {
+        System.out.println("VEPEXETest.testWithUserInputs()");
+        String[] actual = VEPEXE.getVEPCommand( new String[] { "--fork", "4", "-all"} );
+        
+        String[] expected = { 
+        	"", "", "-i", "/dev/stdin", "-o",
+        	"STDOUT", "-dir", "", "-vcf", "--hgnc", 
+        	"-polyphen", "b", "-sift", "b", "--offline",
+        	"--buffer_size", "1",
+        	"--fork", "4",
+        	"-all"
+        };
+        
+        assertVepCmd(expected, actual);
+    }
+
+    
+    private void assertVepCmd(String[] expected, String[] actual) {
         for(int i=0; i < expected.length; i++) {
             // Skip these as they are configurable depending on the system we are running on:
             //	0 (VEP's Perl path)
@@ -35,4 +57,6 @@ public class VEPEXETest {
         	assertEquals("Command element [" + i + "] did not match.", expected[i], actual[i]);
         }
     }
+
+        
 }
