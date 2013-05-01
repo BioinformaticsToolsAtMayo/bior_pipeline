64221db6981e2151f904edba66af481a0adfdac3
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-05-01 20:06:18 +0000

    Refactored VEP code to simplify pipelines

--- src/main/java/edu/mayo/bior/pipeline/VEP/VEPPipeline.java
+++ src/main/java/edu/mayo/bior/pipeline/VEP/VEPPipeline.java
@@ -1,91 +1,84 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.mayo.bior.pipeline.VEP;
 
-import com.tinkerpop.pipes.Pipe;
-import com.tinkerpop.pipes.transform.IdentityPipe;
-import com.tinkerpop.pipes.transform.TransformFunctionPipe;
-import com.tinkerpop.pipes.util.Pipeline;
-import edu.mayo.bior.pipeline.VCFProgramPipes.VCFProgram2HistoryPipe;
-import edu.mayo.bior.pipeline.VCFProgramPipes.VCFProgramMerge;
-import edu.mayo.bior.pipeline.VCFProgramPipes.VCFProgramPreProcessPipe;
-import edu.mayo.exec.AbnormalExitException;
-import edu.mayo.pipes.MergePipe;
-import edu.mayo.pipes.PrintPipe;
-import edu.mayo.pipes.UNIX.CatPipe;
-import edu.mayo.pipes.history.HistoryInPipe;
-import edu.mayo.pipes.history.HistoryOutPipe;
-import edu.mayo.pipes.meta.BridgeOverPipe;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.TimeoutException;
 
+import com.google.gson.JsonArray;
+import com.tinkerpop.pipes.PipeFunction;
+import com.tinkerpop.pipes.transform.TransformFunctionPipe;
+import com.tinkerpop.pipes.util.Pipeline;
+
+import edu.mayo.exec.AbnormalExitException;
+import edu.mayo.pipes.history.ColumnMetaData;
+import edu.mayo.pipes.history.History;
+
 /**
- *
- * @author m102417
+ * @author Michael Meiners (m054457)
+ * Date created: Apr 21, 2013
  */
-public class VEPPipeline extends Pipeline {
-    
-    public VEPPipeline(String[] command) throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
-		init(command, new IdentityPipe(), new IdentityPipe(), true);
-	}
-    
-	public VEPPipeline(String[] command, boolean pickworst) throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
-		init(command, new IdentityPipe(), new IdentityPipe(), pickworst);
-	}
-	public VEPPipeline(Pipe input, Pipe output, boolean pickworst) throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
-		init(VEPEXE.getVEPCommand(null), input, output, pickworst);
-	}
-	public VEPPipeline(String[] command, Pipe input, Pipe output, boolean pickworst) throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{
-		init(command, input, output, pickworst);
-	}
+public class VEPPipeline  extends Pipeline {
 
-	private VEPEXE vep;
-	public void init(String[] command, Pipe input, Pipe output, boolean pickworst) throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{            
-            //execution
-            vep = new VEPEXE(command);
-            Pipe exe = new TransformFunctionPipe(vep);
-            
-            String[] vepHeader = new String[1];
-            vepHeader[0] = "##INFO=<ID=CSQ,Number=.,Type=String,Description=\"Consequence type as predicted by VEP. Format: Allele|Gene|Feature|Feature_type|Consequence|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|HGNC|DISTANCE|SIFT|PolyPhen|CELL_TYPE\">";
-
-            //post processing
-            VEPPostProcessingPipeline ppp = new VEPPostProcessingPipeline();
-            Pipe post;
-            if(pickworst){
-                post = ppp.getWorstCasePipeline(new IdentityPipe(), new IdentityPipe(), false);
-            }else {
-                post = ppp.getCartesianProductPipeline(new IdentityPipe(), new IdentityPipe(),false);
-            }
-
-            Pipeline superviseMe = new Pipeline(
-				new VCFProgramPreProcessPipe(),//history-string 
-				exe,//string-string
-				new VCFProgram2HistoryPipe(vepHeader),//string-history
-				post //history-history
-                        //new IdentityPipe()
-				);
-            BridgeOverPipe bridge = new BridgeOverPipe(superviseMe, new VCFProgramMerge("VEP"));
-
-            Pipeline p = new Pipeline(
-				input,//??-history better convert it to a history comming into the pipeline e.g. HistoryInPipe
-				bridge, //the BridgeOverPipe: history-history
-				output
-				//new PrintPipe()
-				);
-            this.setPipes(p.getPipes());
-	}
-        
-        
-        
+	private VEPEXE mVepExe = null;
+	private boolean mIsWorstCaseOnly = true;
+	private Queue<History>  mLastLineQ = new ArrayDeque<History>(); 
 	
+	public VEPPipeline(String[] command, boolean pickworst) throws IOException, InterruptedException, BrokenBarrierException, TimeoutException, AbnormalExitException{            
+        mIsWorstCaseOnly = pickworst;
+        mVepExe = new VEPEXE(VEPEXE.getVEPCommand(new String[] {"1"} ));
+        Pipeline pipeline = new Pipeline(
+        		new TransformFunctionPipe(new VepPrePipe()),
+        		new TransformFunctionPipe(mVepExe),
+        		new TransformFunctionPipe(new VepPostPipe())
+        		);
+        this.setPipes(pipeline.getPipes());
+	}
 	
+	class VepPrePipe implements PipeFunction<History,String> {
+		@Override
+		/** Save the input line to a queue and strip off all columns after column 7 */
+		public String compute(History hist) {
+			mLastLineQ.add((History) hist.clone());
+			for(int i=hist.size()-1; i > 6; i--) 
+				hist.remove(i);
+			return hist.getMergedData("\t");
+		}
+	}
+
+	class VepPostPipe implements PipeFunction<String,History> {
+		private VepFunctions mVepFunctions = new VepFunctions();
+		private boolean isFirstLine = true;
+		
+		@Override
+		/** Create a json array from VEP output, convert it to array, and if mIsWorstCaseOnly is set, 
+		 *  then return the worst element from the array. 
+		 * 	If there are no scores to compare, then return empty json. */
+		public History compute(String histFromVep) {
+			addHeaderMetaData();
+			String vepCsq = histFromVep.split("\t")[7];
+			JsonArray csqAsJsonArray = mVepFunctions.vepCsqToJsonList(vepCsq);
+			String vepOut = mIsWorstCaseOnly 
+					? mVepFunctions.getWorstCase(csqAsJsonArray).toString() 
+					: csqAsJsonArray.toString();
+			History original = mLastLineQ.remove();
+			original.add(vepOut);
+			return original;
+		}
+
+		/** Add "VEP" to the end of the column header row */
+		private void addHeaderMetaData() {
+			if( isFirstLine ) {
+				isFirstLine = false;
+				History.getMetaData().getColumns().add(new ColumnMetaData("VEP"));
+			}
+		}
+	}
+
 	// NOTE: Make sure to call this terminate() method when the pipe is finished!!!!!
     public void terminate() throws InterruptedException{
-        vep.terminate();
+        mVepExe.terminate();
     }
-    
 }
---
+++ src/main/java/edu/mayo/bior/pipeline/VEP/VepFunctions.java
@@ -0,0 +1,123 @@
+package edu.mayo.bior.pipeline.VEP;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+/**
+ * @author Michael Meiners (m054457)
+ * Date created: Apr 25, 2013
+ */
+public class VepFunctions {
+	private final static String[] headers = {
+		"Allele",
+		"Gene",
+		"Feature",
+		"Feature_type",
+		"Consequence",
+		"cDNA_position",
+		"CDS_position",
+		"Protein_position",
+		"Amino_acids",
+		"Codons",
+		"Existing_variation",
+		"HGNC",
+		"DISTANCE",
+		"SIFT",
+		"PolyPhen",
+		"CELL_TYPE"
+	};
+	
+	class Effect {
+		public String term;
+		public Double score;
+	}
+	
+	/** Turn CSQ result ("CSQ=x|y|z") into json ("{CSQ:{"a":"x","b":"y","c":"z"}}")  */
+	public JsonArray vepCsqToJsonList(String vepCsq) {
+		String[] vepCsqItems = vepCsq.replace("CSQ=", "").split(",");
+		JsonArray jsonArray = new JsonArray();
+		for(String csqItem : vepCsqItems) {
+			jsonArray.add(vepCsqToJson(csqItem));
+		}
+		return jsonArray;
+	}
+	
+	
+	/** Turn CSQ result into JSON
+	 *  Ex CSQ: "A|ENSG00000154719|ENST00000307301|Transcript|missense_variant|1043|1001|334|T/M|aCg/aTg||MRPL39||tolerated(0.05)|benign(0.001)|"
+	 *  Ex JSON:"{"Allele":"A","Gene":"ENSG00000154719","Feature":"ENST00000307301","Feature_type":"Transcript","Consequence":"missense_variant","cDNA_position":"1043","CDS_position":"1001","Protein_position":"334","Amino_acids":"T/M","Codons":"aCg/aTg","HGNC":"MRPL39","SIFT":"tolerated(0.05)","PolyPhen":"benign(0.001)","SIFT_TERM":"tolerated","SIFT_Score":0.05,"PolyPhen_TERM":"benign","PolyPhen_Score":0.001}"  */
+	private JsonObject vepCsqToJson(String vepCsqItem) {
+		String[] split = vepCsqItem.split("\\|");
+		JsonObject jsonObj = new JsonObject();
+
+		// NOTE: the split ignores any empty columns at the end, so there may be fewer than 16 after split 
+		// For ex: "a||b|||" will only have 3 columns
+		for(int i=0; i < Math.min(split.length, headers.length); i++) {
+			if( ! "".equals(split[i]))
+				jsonObj.addProperty(headers[i], split[i]);
+		}
+		
+		boolean isSiftPresent = false;
+		if( split.length > 13 && split[13].contains("(") && split[13].contains(")") ) {
+			isSiftPresent = true;
+			Effect sift = parseEffect(split[13]);
+			jsonObj.addProperty("SIFT_TERM", sift.term);
+			jsonObj.addProperty("SIFT_Score", sift.score);
+		}
+		
+		// TODO: These had been the defaults - are they necessary?  Should we populate polyphen with defaults if not given?
+		//Double pscore = 0.0;
+		//String pterm = "not_predicted";
+		if( split.length > 14 && split[14].contains("(") && split[14].contains(")") ) {
+			Effect polyphen = parseEffect(split[14]);
+			jsonObj.addProperty("PolyPhen_TERM", polyphen.term);
+			jsonObj.addProperty("PolyPhen_Score", polyphen.score);
+		} // else if( isSiftPresent ) {
+			// set default values for polyphen
+		//}
+		return jsonObj;
+	}
+
+	/** Convert string (ex: "tolerated(0.05)") to an Effect object */
+	private Effect parseEffect(String effStr) {
+		int idxOpenParen = effStr.indexOf("(");
+		int idxCloseParen= effStr.indexOf(")");
+		
+		if(idxOpenParen == -1 || idxCloseParen == -1 )
+			return null;
+		
+		Effect effect = new Effect();
+		effect.term = effStr.substring(0, idxOpenParen);
+		effect.score = Double.parseDouble(effStr.substring(idxOpenParen+1, idxCloseParen));
+		return effect;
+	}
+	
+	/** Given a list of vepCsqOutputs as JSON strings, return the one that has the worst outcome */
+	public JsonObject getWorstCase(JsonArray jsonArray) {
+		Double worstSift = 1.0;
+		Double worstPoly = Double.MAX_VALUE;
+
+		// NOTE: if the worstJson object is never set, it will be converted to string "{}"
+		JsonObject worstJson = new JsonObject();
+		
+		// TODO: What if EITHER sift or polyphen is not given???????????
+		for(int i=0; i < jsonArray.size(); i++) {
+			JsonObject jsonObj = jsonArray.get(i).getAsJsonObject();
+			JsonElement siftScore = jsonObj.get("SIFT_Score");
+			JsonElement polyphenScore = jsonObj.get("PolyPhen_Score");
+			// Ignore this item if it does not have a score to compare
+			if(siftScore == null || polyphenScore == null)
+				continue;
+			// If we don't have a worst yet, then add this one, else compare the scores to see if this one's is worse
+			if(worstJson.entrySet().isEmpty()  ||  (siftScore.getAsDouble() <= worstSift && polyphenScore.getAsDouble() >= worstPoly) ){
+				worstSift = siftScore.getAsDouble();
+				worstPoly = polyphenScore.getAsDouble();
+				worstJson = jsonObj;
+			}
+		}
+		
+		return worstJson;
+	}
+	
+}
