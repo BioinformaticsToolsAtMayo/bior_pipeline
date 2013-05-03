70a4feed2ab86af5277edb37ff1b25d6512c66aa
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-05-03 05:40:29 +0000

    implemented

--- src/main/java/edu/mayo/bior/pipeline/Treat/format/OmimFormatter.java
+++ src/main/java/edu/mayo/bior/pipeline/Treat/format/OmimFormatter.java
@@ -1,34 +1,31 @@
 package edu.mayo.bior.pipeline.Treat.format;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.jayway.jsonpath.JsonPath;
 
 import edu.mayo.bior.pipeline.Treat.JsonColumn;
 
 public class OmimFormatter implements Formatter
 {
+	// Compile the json paths initially as these will be called repeatedly - once per line
+	private static final JsonPath PATH_ID      = JsonPath.compile("MIM_Number");
+	private static final JsonPath PATH_DISEASE = JsonPath.compile("Disorders");
+
 	public JsonColumn getJSONColumn() {
 		return JsonColumn.OMIM;
 	}
 	
-	public List<String> getHeaders()
-	{
-		List<String> headers = new ArrayList<String>();
-		
-		// TODO implement
-		headers.add("TODO_" + getClass().getName());
-		
-		return headers;
+	public List<String> getHeaders()  {
+		return Arrays.asList("OMIM.ID", "OMIM.Disease");
 	}
 	
 	public List<String> format(String json)
 	{
-		List<String> values = new ArrayList<String>();
-		
-		// TODO: implement
-		values.add("TODO");
-
-		return values;
+		return Arrays.asList(
+			FormatUtils.drill(PATH_ID, json),
+			FormatUtils.drill(PATH_DISEASE, json)
+		);
 	}
-
 }
