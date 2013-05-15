58a112fe3d9b7f8e1d2dffc71cc2702e2f485207
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-05-15 14:05:27 +0000

    Added case for testing case sensitivity in lookup

--- src/test/java/edu/mayo/bior/cli/func/LookupCommandITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/LookupCommandITCase.java
@@ -24,6 +24,9 @@ public class LookupCommandITCase extends BaseFunctionalTest {
         String header = getHeader(out.stdout);
 
         // pull out just data rows
+        // Ex output: Y	28740815	28780802	{"_type":"gene","_landmark":"Y","_strand":"-","_minBP":28740815,"_maxBP":28780802,
+        //           "gene":"PARP4P1","gene_synonym":"ADPRTL1P; PARP4P; PARP4PY1","note":"poly (ADP-ribose) polymerase family, member 4 pseudogene 1; Derived by automated computational analysis using gene prediction method: Curated Genomic.",
+        //           "pseudo":"","GeneID":"347613","HGNC":"18500"}
 	    String data = out.stdout.replace(header, "");
 	    String[] cols = data.split("\t");
 
@@ -32,6 +35,27 @@ public class LookupCommandITCase extends BaseFunctionalTest {
 	    assertEquals("PARP4P1", JsonPath.compile("gene").read(json));	    
 	    assertEquals("347613", JsonPath.compile("GeneID").read(json));
 	}
+
+	@Test
+	public void testMatchCaseInsensitive() throws IOException, InterruptedException {
+		String stdin = "src/test/resources/genes.tsv.bgz";
+
+	    String inputLine = "Parp4p1"; //"PARP4P1";
+	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "gene", "-d", stdin);
+        assertEquals(out.stderr, 0, out.exit);
+        assertEquals("", out.stderr);
+
+        String header = getHeader(out.stdout);
+
+        // pull out just data rows
+	    String data = out.stdout.replace(header, "");
+	    String[] cols = data.split("\t");
+
+	    String json = cols[cols.length - 1];
+	    
+	    assertEquals("PARP4P1", JsonPath.compile("gene").read(json));	    
+	}
+
 	
 	@Test
 	public void testNoMatch() throws IOException, InterruptedException {
