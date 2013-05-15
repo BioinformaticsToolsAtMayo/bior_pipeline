394b17c0483b6ff9d8f705cd1e7d2867b02ff0ac
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-05-15 21:51:27 +0000

    Updated with latest gold file from Daniel and column-by-column checking logic in TreatITCase.

--- src/test/java/edu/mayo/bior/cli/func/remoteexec/TreatITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/remoteexec/TreatITCase.java
@@ -31,20 +31,59 @@ public class TreatITCase extends RemoteFunctionalTest
 		String goldInput  = FileUtils.readFileToString(new File("src/test/resources/treat/gold.vcf"));
 		String goldOutput = FileUtils.readFileToString(new File("src/test/resources/treat/gold_output.tsv"));
 		
-		CommandOutput out = executeScript("bior_annotate", goldInput);
+		CommandOutput out = executeScript("bior_annotate", goldInput, "--log");
 
 		// check that command completed successfully
-		if (out.exit != 0) {
+		if (out.exit != 0)
+		{
 			fail(out.stderr);
 		}
 		
-		List<String> expectedOutputLines = splitLines(goldOutput);
-		List<String> actualOutputLines = splitLines(out.stdout);
+		List<String> expectedLines = splitLines(goldOutput);
+		List<String> actualLines = splitLines(out.stdout);
+
+		// check number of rows
+		assertEquals(
+			String.format("# of rows did not match.", expectedLines.size(), actualLines.size()), 
+			expectedLines.size(),
+			actualLines.size());
 		
-		// compare line-by-line
-		assertEquals(expectedOutputLines.size(), actualOutputLines.size());
-		for (int i=0; i < expectedOutputLines.size(); i++) {
-			assertEquals(expectedOutputLines.get(i), actualOutputLines.get(i));
+		for (int i=0; i < expectedLines.size(); i++)
+		{
+			final int lineNum = i + 1;
+			
+			String expectedLine = expectedLines.get(i);
+			String actualLine   = actualLines.get(i);
+			
+			String[] expectedCols = expectedLine.split("\t");
+			String[] actualCols   = actualLine.split("\t");
+			
+			// check number of columns
+			assertEquals(
+				String.format("# of columns did not match for line %s.", lineNum, expectedCols.length, actualCols.length), 
+				expectedCols.length,
+				actualCols.length);
+			
+			// compare column-by-column
+			for (int col=0; col < expectedCols.length; col++)
+			{
+				final int colNum = col + 1;
+				
+				String expectedVal = expectedCols[col];
+				String actualVal   = actualCols[col]; 
+				
+				// compare ONLY if the value is not "*"
+				if (expectedVal.equals("*") == false)
+				{
+					// compare column values
+					assertEquals(
+						String.format(
+								"Values did not match for line %s, column %s.\n" + "expected line: %s\n" + "actual line: %s\n",
+								lineNum, colNum, expectedLine, actualLine),
+						expectedVal,
+						actualVal);
+				}
+			}
 		}
 	}
 	
