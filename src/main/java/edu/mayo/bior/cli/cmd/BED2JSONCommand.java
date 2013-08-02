456a9af13656f5f7496099eb7576958b772e5b80
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-02 19:22:13 +0000

    Integrated with new HistoryInPipe constructor

--- src/main/java/edu/mayo/bior/cli/cmd/BED2JSONCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/BED2JSONCommand.java
@@ -4,13 +4,21 @@
  */
 package edu.mayo.bior.cli.cmd;
 
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
+import com.tinkerpop.pipes.Pipe;
+
 import edu.mayo.bior.pipeline.UnixStreamPipeline;
 import edu.mayo.cli.CommandPlugin;
 import edu.mayo.pipes.bioinformatics.BED2JSONPipe;
-import edu.mayo.pipes.bioinformatics.VCF2VariantPipe;
-import java.util.Properties;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Options;
+import edu.mayo.pipes.history.History;
+import edu.mayo.pipes.history.HistoryInPipe;
+import edu.mayo.pipes.history.HistoryOutPipe;
+import edu.mayo.pipes.util.metadata.Metadata;
+import edu.mayo.pipes.util.metadata.Metadata.CmdType;
 
 /**
  *
@@ -19,14 +27,20 @@ import org.apache.commons.cli.Options;
 public class BED2JSONCommand implements CommandPlugin {
 
     	private UnixStreamPipeline mPipeline = new UnixStreamPipeline();
+    	private String operation;
 	
 	public void init(Properties props) throws Exception {
+		operation = props.getProperty("command.name");
 	}
 
 	public void execute(CommandLine line, Options opts) throws Exception {
 		
-		BED2JSONPipe pipe = new BED2JSONPipe();
+		Metadata metadata = new Metadata(CmdType.ToTJson, operation);
 		
-		mPipeline.execute(pipe);		
+		Pipe<String,  History>  preLogic  = new HistoryInPipe(metadata);
+		Pipe<History, History>  logic     = new BED2JSONPipe();
+		Pipe<History, String>   postLogic = new HistoryOutPipe();
+		
+		mPipeline.execute(preLogic, logic, postLogic);
 	}
 }
