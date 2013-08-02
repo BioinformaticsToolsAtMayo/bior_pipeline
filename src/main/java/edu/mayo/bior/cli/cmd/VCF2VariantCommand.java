8ab399f40b599e7168b0690de50389e63c7560d1
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-02 19:15:58 +0000

    Integrated with new HistoryInPipe constructor

--- src/main/java/edu/mayo/bior/cli/cmd/VCF2VariantCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/VCF2VariantCommand.java
@@ -5,22 +5,37 @@ import java.util.Properties;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 
+import com.tinkerpop.pipes.Pipe;
+
 import edu.mayo.bior.pipeline.UnixStreamPipeline;
 import edu.mayo.cli.CommandPlugin;
 import edu.mayo.pipes.bioinformatics.VCF2VariantPipe;
+import edu.mayo.pipes.history.History;
+import edu.mayo.pipes.history.HistoryInPipe;
+import edu.mayo.pipes.history.HistoryOutPipe;
+import edu.mayo.pipes.util.metadata.Metadata;
+import edu.mayo.pipes.util.metadata.Metadata.CmdType;
 
-public class VCF2VariantCommand implements CommandPlugin {
-
-	private UnixStreamPipeline mPipeline = new UnixStreamPipeline();
+public class VCF2VariantCommand implements CommandPlugin
+{
 	
-	public void init(Properties props) throws Exception {
+	private UnixStreamPipeline pipeline = new UnixStreamPipeline();
+	
+	private String operation;
+	
+	public void init(Properties props) throws Exception
+	{
+		operation = props.getProperty("command.name");
 	}
 
-	public void execute(CommandLine line, Options opts) throws Exception {
+	public void execute(CommandLine line, Options opts) throws Exception
+	{
+		Metadata metadata = new Metadata(CmdType.ToTJson, operation);
 		
-		VCF2VariantPipe pipe = new VCF2VariantPipe();
+		Pipe<String,  History>  preLogic  = new HistoryInPipe(metadata);
+		Pipe<History, History>  logic     = new VCF2VariantPipe();
+		Pipe<History, String>   postLogic = new HistoryOutPipe();
 		
-		mPipeline.execute(pipe);		
+		pipeline.execute(preLogic, logic, postLogic);
 	}
 }
-
