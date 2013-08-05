65fe3e2a54b2e8b849443e7dc7cfafb40667e07a
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-05 20:19:00 +0000

    Updated to check for latest metadata line spec and column name.

--- src/test/java/edu/mayo/bior/cli/func/LookupCommandITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/LookupCommandITCase.java
@@ -3,32 +3,34 @@ package edu.mayo.bior.cli.func;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 
 import com.jayway.jsonpath.JsonPath;
 
-import edu.mayo.bior.cli.cmd.LookupCommand;
-import edu.mayo.cli.InvalidDataException;
-import edu.mayo.cli.InvalidOptionArgValueException;
-
 public class LookupCommandITCase extends BaseFunctionalTest {
 
 	@Test
-	public void testMatch() throws IOException, InterruptedException {
-		String stdin = "src/test/resources/genes.tsv.bgz";
+	public void testMatch() throws IOException, InterruptedException
+	{
+		String catRelativePath = "src/test/resources/genes.tsv.bgz";
+        String catCanonicalPath = (new File(catRelativePath)).getCanonicalPath();        
+        
+	    String stdin = 
+	    		"#CHROM\tSTART\tEND\tHGNC_ID\n" +
+	    		"Y\t28740815\t28780802\t18500";
 
-	    String inputLine = "Y\t28740815\t28780802\t18500";
-
-	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNC", "-d", stdin);
+	    CommandOutput out = executeScript("bior_lookup", stdin, "-p", "HGNC", "-d", catRelativePath);
         assertEquals(out.stderr, 0, out.exit);
         assertEquals("", out.stderr);
 
         String header = getHeader(out.stdout);
+        String[] headerLines = header.split("\n");
+        assertEquals(2, headerLines.length);
+        assertEquals(String.format("##BIOR=<ID=\"bior.genes\",Operation=\"bior_lookup\",DataType=\"JSON\",ShortUniqueName=\"genes\",Path=\"%s\"", catCanonicalPath), headerLines[0]);
+        assertEquals("#CHROM\tSTART\tEND\tHGNC_ID\tbior.genes", headerLines[1]);         
 
         // pull out just data rows
         // Ex output: Y	28740815	28780802	{"_type":"gene","_landmark":"Y","_strand":"-","_minBP":28740815,"_maxBP":28780802,
@@ -43,6 +45,35 @@ public class LookupCommandITCase extends BaseFunctionalTest {
 	    assertEquals("347613", JsonPath.compile("GeneID").read(json));
 	}
 
+	@Test
+	public void testNoMatch() throws IOException, InterruptedException
+	{
+		String catRelativePath = "src/test/resources/genes.tsv.bgz";
+        String catCanonicalPath = (new File(catRelativePath)).getCanonicalPath();        
+
+	    String inputLine =
+	    		"#CHROM\tSTART\tEND\tHGNC_ID\n" +
+	    		"Y\t28740815\t28780802\t9999999";
+
+	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNC", "-d", catRelativePath);
+        assertEquals(out.stderr, 0, out.exit);
+        assertEquals("", out.stderr);
+
+        String header = getHeader(out.stdout);
+        String[] headerLines = header.split("\n");
+        assertEquals(2, headerLines.length);
+        assertEquals(String.format("##BIOR=<ID=\"bior.genes\",Operation=\"bior_lookup\",DataType=\"JSON\",ShortUniqueName=\"genes\",Path=\"%s\"", catCanonicalPath), headerLines[0]);
+        assertEquals("#CHROM\tSTART\tEND\tHGNC_ID\tbior.genes", headerLines[1]);         
+
+        // pull out just data rows
+	    String data = out.stdout.replace(header, "");
+	    String[] cols = data.split("\t");
+
+	    assertEquals("{}", cols[4].trim());
+	    
+	    
+	}	
+	
 	@Test
 	public void testMatchCaseInsensitive() throws IOException, InterruptedException {
 	    String inputLine = "Parp4p1"; // Actual name is: "PARP4P1";
@@ -80,37 +111,15 @@ public class LookupCommandITCase extends BaseFunctionalTest {
 	    
 	    // Should NOT be found since the case didn't match that in the database
 	    assertEquals("{}", json);
-	}
-	
-	@Test
-	public void testNoMatch() throws IOException, InterruptedException {
-		String stdin = "src/test/resources/genes.tsv.bgz";
-
-	    String inputLine = "Y\t28740815\t28780802\t9999999";
-
-	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNC", "-d", stdin);
-        assertEquals(out.stderr, 0, out.exit);
-        assertEquals("", out.stderr);
-
-        String header = getHeader(out.stdout);
-
-        // pull out just data rows
-	    String data = out.stdout.replace(header, "");
-	    String[] cols = data.split("\t");
-
-	    assertEquals("{}", cols[4].trim());
-	    
-	    
-	}
-	
+	}	
 	
 	@Test
 	public void testInvalidInput() throws IOException, InterruptedException {
-		String stdin = "src/test/resources/genes.tsv.bgz";
+		String catRelativePath = "src/test/resources/genes.tsv.bgz";
 
 	    String inputLine = "Y\t28740815\t28780802\tJUNK";
 
-	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNC", "-d", stdin);
+	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNC", "-d", catRelativePath);
         assertEquals(out.stderr, 0, out.exit);
         assertEquals("", out.stderr);
 
@@ -125,23 +134,23 @@ public class LookupCommandITCase extends BaseFunctionalTest {
 	
 	@Test
 	public void testDefaultIndexFileNotExist() throws IOException, InterruptedException {
-		String stdin = "src/test/resources/genes.tsv.bgz";
+		String catRelativePath = "src/test/resources/genes.tsv.bgz";
 
 	    String inputLine = "Y\t28740815\t28780802\tJUNK";
 
-	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNCId", "-d", stdin);
+	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNCId", "-d", catRelativePath);
         assertEquals(out.stderr, 1, out.exit);
         assertTrue(out.stderr.contains("The built-in index for")  &&  ! out.stderr.contains("The index file path you specified"));
 	}
 	
 	@Test
 	public void testUserIndexFileNotExist() throws IOException, InterruptedException {
-		final String STDIN = "src/test/resources/genes.tsv.bgz";
+		final String catRelativePath = "src/test/resources/genes.tsv.bgz";
 		final String USER_IDX = "doesNotExist.h2.db";
 
 	    String inputLine = "Y\t28740815\t28780802\tJUNK";
 
-	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNCId", "-d", STDIN, "-i", USER_IDX);
+	    CommandOutput out = executeScript("bior_lookup", inputLine, "-p", "HGNCId", "-d", catRelativePath, "-i", USER_IDX);
         assertEquals(out.stderr, 1, out.exit);
         assertTrue( ! out.stderr.contains("The built-in index for")  &&  out.stderr.contains("The index file path you specified does not exist:"));
 	}
