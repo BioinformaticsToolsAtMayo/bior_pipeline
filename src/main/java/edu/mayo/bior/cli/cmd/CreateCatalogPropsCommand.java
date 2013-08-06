cb5ff6e0fc0eb0bf81e056efbac05c9d2a0fa796
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-06 23:15:30 +0000

    Code and tests for bior_create_catalog_properties command -------------------------- CreateCatalogPropsCommand.java: datasource properties file should only output 5 main properties lines;  created execNoCmd() method to allow run of tests without cmd line execution; Scans ALL lines in catalog now; Walks ALL JSON paths (instead of just INFO) recursively; Loads default descriptions for known BioR and VCF fields AddBiorMetadataLinesToHeaderPipe.java:  Removed (duplicate of other code) PropertiesFileUtil.java: Removed (duplicate of same class in pipes project) allCatalogs.columns.properties:  Added default column descriptions for BioR and VCF fields CreateCatalogPropsCommandITCase.java: Added test for direct call (not using command execution call); Added test for multi-level JSON paths AddBiorMetadataLinesToHeaderPipeTest.java: Removed.  No longer needed since other tests cover this src/test/resources/metadata/testJsonDepth.*: Created test catalog; Created expected output properties files src/test/resources/genes.*.properties.expected: Created expected output properties files from genes test catalog

--- src/main/java/edu/mayo/bior/cli/cmd/CreateCatalogPropsCommand.java
+++ src/main/java/edu/mayo/bior/cli/cmd/CreateCatalogPropsCommand.java
@@ -11,24 +11,34 @@ import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.tinkerpop.pipes.util.Pipeline;
 
 import edu.mayo.cli.CommandPlugin;
 import edu.mayo.cli.InvalidDataException;
 import edu.mayo.cli.InvalidOptionArgValueException;
 import edu.mayo.pipes.UNIX.CatGZPipe;
+import edu.mayo.pipes.util.PropertiesFileUtil;
 import edu.mayo.pipes.util.metadata.AddMetadataLines.BiorMetaControlledVocabulary;
 
+/** Given a catalog path, construct the metadata files associated with that catalog:
+ *  <catalog>.datasource.properties
+ *  <catalog>.columns.properties
+ *  
+ * @author Surendra Konathala, Michael Meiners (m054457)
+ * Date created: Aug 1, 2013
+ */
 public class CreateCatalogPropsCommand implements CommandPlugin {
 
 	// Catalog path
-	private static final char OPTION_CATALOG = 'd';
+	public static final char OPTION_CATALOG = 'd';
 	
 	//BIOR vocabulary is defined here
 	private List<BiorMetaControlledVocabulary> biorMetaControlledVocabulary = Arrays.asList(BiorMetaControlledVocabulary.values());
@@ -36,12 +46,19 @@ public class CreateCatalogPropsCommand implements CommandPlugin {
 	public void init(Properties props) throws Exception {
 	}
 	
-	public void execute(CommandLine line, Options opts) throws InvalidOptionArgValueException, InvalidDataException, IOException {		
+	public void execute(CommandLine line, Options opts) throws InvalidOptionArgValueException, InvalidDataException, IOException {	
 		// Catalog path and key are required		
-		String bgzipPath = line.getOptionValue(OPTION_CATALOG);		
-		//String bgzipPath = "src/test/resources/ESPFuncTest.tsv.bgz";
-		
-		File catalogFile = new File(bgzipPath);
+		String catalogBgzipPath = line.getOptionValue(OPTION_CATALOG);
+		Option catalogOption = opts.getOption(OPTION_CATALOG + "");
+
+		execNoCmd(catalogBgzipPath, catalogOption);
+	}
+
+	public void execNoCmd(String catalogBgzipPath, Option catalogOption)
+			throws InvalidOptionArgValueException, InvalidDataException,
+			IOException
+	{
+		File catalogFile = new File(catalogBgzipPath);
 		
 		if( catalogFile.exists() ) {
 			String catalogFullFilename = catalogFile.getName();
@@ -49,16 +66,16 @@ public class CreateCatalogPropsCommand implements CommandPlugin {
 			String catalogFilePath = catalogFile.getParent();
 			
 			// Create the datasource.props file
-			createDatasourcePropsFile(opts, catalogFile, catalogFilename, catalogFilePath);
+			createDatasourcePropsFile(catalogOption, catalogFile, catalogFilename, catalogFilePath);
 			
 			// Create the columns.props file
-			createColumnPropsFile(opts, catalogFile, catalogFilename, catalogFilePath);
+			createColumnPropsFile(catalogOption, catalogFile, catalogFilename, catalogFilePath);
 			
 		} else {
 			throw new InvalidOptionArgValueException(
-				opts.getOption(OPTION_CATALOG + ""),				
-				bgzipPath,
-				"Catalog file could not be found: " + bgzipPath
+				catalogOption,				
+				catalogBgzipPath,
+				"Catalog file could not be found: " + catalogBgzipPath
 			);
 		}
 	}
@@ -72,38 +89,41 @@ public class CreateCatalogPropsCommand implements CommandPlugin {
 	 * @throws InvalidDataException
 	 * @throws IOException
 	 */
-	protected void createDatasourcePropsFile(Options opts, File catalogFile, String catalogFilename, String catalogFilePath) throws InvalidOptionArgValueException, InvalidDataException, IOException {
+	protected void createDatasourcePropsFile(Option catalogOption, File catalogFile, String catalogFilename, String catalogFilePath) throws InvalidOptionArgValueException, InvalidDataException, IOException {
 		
 		//Check to see if "catalogFilePath" is WRITABLE
 		File dir = new File(catalogFilePath);
 		if (!dir.exists() || !dir.isDirectory() || !dir.canWrite()) {
 			throw new InvalidOptionArgValueException(
-				opts.getOption(OPTION_CATALOG + ""),				
+				catalogOption,				
 				catalogFile.getCanonicalPath(),
 				"Source file directory doesn't exist or is not writable: " + catalogFile.getCanonicalPath()					
 			);
 	    }
 		
 		//Create the props file
-		File datasourcePropsFile = new File(catalogFilePath + catalogFile.separator + catalogFilename+".datasource"+".properties");
+		File datasourcePropsFile = new File(catalogFilePath + catalogFile.separator + catalogFilename + ".datasource" + ".properties");
 		
 		if (datasourcePropsFile.exists()) {
 			throw new InvalidOptionArgValueException(
-				opts.getOption(OPTION_CATALOG + ""),				
+				catalogOption,				
 				catalogFile.getCanonicalPath(),
 				"Datasource properties file already exists for this catalog - " + catalogFile.getCanonicalPath()
 			);
-		}		
+		}
 		datasourcePropsFile.createNewFile();
 		//System.out.println(datasourcePropsFile.exists());
 		
 		//write all values from the enum to the file
-		List<String> dsAttribs = new ArrayList<String>();
-		for (BiorMetaControlledVocabulary val : biorMetaControlledVocabulary) {
-			dsAttribs.add(val.toString()+"=");
-		}
+		List<String> dsAttribs = Arrays.asList(
+				BiorMetaControlledVocabulary.SHORTNAME.toString(),
+				BiorMetaControlledVocabulary.DESCRIPTION.toString(),
+				BiorMetaControlledVocabulary.SOURCE.toString(),
+				BiorMetaControlledVocabulary.VERSION.toString(),
+				BiorMetaControlledVocabulary.BUILD.toString()
+				);
 
-		String content = buildContent(catalogFilename, "Datasource", dsAttribs);
+		String content = buildContent(catalogFilename, "Datasource", dsAttribs, null);
 		this.writeToFile(datasourcePropsFile, content);
 		//return true;
 	}
@@ -118,61 +138,70 @@ public class CreateCatalogPropsCommand implements CommandPlugin {
 	 * @throws InvalidDataException
 	 * @throws IOException
 	 */
-	protected void createColumnPropsFile(Options opts,File catalogFile, String catalogFilename, String catalogFilePath) throws InvalidOptionArgValueException, InvalidDataException, IOException {
+	protected void createColumnPropsFile(Option catalogOption,File catalogFile, String catalogFilename, String catalogFilePath) throws InvalidOptionArgValueException, InvalidDataException, IOException {
 		
 		Pipeline pipe = new Pipeline(new CatGZPipe("gzip"));
 		pipe.setStarts(Arrays.asList(catalogFile.getPath()));
 		
-		String jsonLine = null;
+		List<String> allJsonKeys = new ArrayList<String>();
 		while(pipe.hasNext()) {
-			String nextVal = (String)pipe.next();
-			if (!nextVal.startsWith("#")) {
-				String[] aVal = nextVal.split("\t");
-				jsonLine = aVal[3];
-				//System.out.println(jsonLine);
-				
-				break;
-			}			
+			String line = (String)pipe.next();
+			
+			// Skip the line if it is blank, or is a header line (starts with "#")
+			if( line.trim().length() == 0  ||  line.startsWith("#"))
+				continue;
+
+			String[] aVal = line.split("\t");
+			String jsonStr = aVal[3];
+			List<String> jsonPaths = getJsonKeys(jsonStr, "");
+			// Add all json paths to the full list
+			for(String jsonPath : jsonPaths) {
+				// If not found in the list yet, add it
+				if( ! allJsonKeys.contains(jsonPath) )
+					allJsonKeys.add(jsonPath);
+			}
 		}
 		
-		List<String> jKeys = new ArrayList<String>();
-	    JsonObject root = new JsonParser().parse(jsonLine).getAsJsonObject();
-	    
-	    for (Map.Entry<String,JsonElement> entry : root.entrySet()) {
-	    	String key = entry.getKey();
-            JsonElement value = entry.getValue();
-	    	
-	    	if (key.contains("INFO")) {
-	    		JsonArray jarr = new JsonArray();
-	    		jarr.add(value);
-	    		
-	    		//System.out.println(jarr.get(0));
-	    		JsonObject obj1 = jarr.get(0).getAsJsonObject();
-	    		for (Map.Entry<String,JsonElement> infokey : obj1.entrySet()) {
-	    			jKeys.add("INFO."+infokey.getKey());
-	    		}	    		
-	    	} else {
-	    		jKeys.add(key+"=");
-	    	}	    	
-	    }    
-
 	    //Create the props file
 	    File columnPropsFile = new File(catalogFilePath + catalogFile.separator + catalogFilename+".columns"+".properties");
 	    if (columnPropsFile.exists()) {
 			throw new InvalidOptionArgValueException(
-				opts.getOption(OPTION_CATALOG + ""),
+				catalogOption,
 				catalogFilePath,
 				"Column properties file already exists for this catalog - " + catalogFilePath
 			);
 		}	
 	    
 	    columnPropsFile.createNewFile();
-		String content = buildContent(catalogFilename, "Column", jKeys);
+	    Properties defaults = new PropertiesFileUtil("src/main/resources/allCatalogs.columns.properties").getProperties();
+		String content = buildContent(catalogFilename, "Column", allJsonKeys, defaults);
 		this.writeToFile(columnPropsFile, content);			
 		//return true;		
 	}
 	
 	
+	/** Given a JSON string, pull out all keys.  Ex: {"CHR":1,"POS":111,"INFO":{"SAO":1.0,"SSR":2.0}}<br>
+	 *  Return:  CHR, POS, INFO.SAO, INFO.SSR
+	 * @param jsonStr The full JSON string to search
+	 * @return A list of all leaf keys in the JSON string 
+	 */
+	private List<String> getJsonKeys(String jsonStr, String parentJsonPath) {
+		List<String> jsonKeys = new ArrayList<String>();
+	    JsonObject root = new JsonParser().parse(jsonStr).getAsJsonObject();
+	    
+	    for (Map.Entry<String,JsonElement> entry : root.entrySet()) {
+	    	String key = (parentJsonPath == null || parentJsonPath.length() == 0  ?  ""  :  parentJsonPath + ".") + entry.getKey();
+            JsonElement value = entry.getValue();
+	    	
+            // If it is a complex object, then break it down further
+            if(value instanceof JsonPrimitive)
+	    		jsonKeys.add(key);
+            else
+            	jsonKeys.addAll(getJsonKeys(value.getAsJsonObject().toString(), key));
+	    }
+	    return jsonKeys;
+	}
+
 	/**
 	 * Write content to a file
 	 * @param source
@@ -192,18 +221,19 @@ public class CreateCatalogPropsCommand implements CommandPlugin {
 	 * @param catalogFilename
 	 * @param type - Datasource or Columns
 	 * @param listValues
+	 * @param defaultDescriptions The descriptions (values) to associate with known keys.  This is useful for setting common column properties such as the bior properties "_landmark", "_minBP", etc, or VCF-columns such as "CHROM", or "POS".
 	 * @return
 	 */
-	private String buildContent(String catalogFilename, String type, List<String> listValues) {
+	private String buildContent(String catalogFilename, String type, List<String> listValues, Properties defaultDescriptions) {
 		StringBuilder sb = new StringBuilder();
 		
 		sb.append("# " + type + " properties file for Catalog - ");
 		sb.append(catalogFilename);
 		sb.append(". Please fill in the description to the below keys.\n");
 		
-		for(int i=0;i<listValues.size();i++) {
-			sb.append(listValues.get(i));
-			sb.append("\n");
+		for(String key : listValues) {
+			String desc = defaultDescriptions == null  ?  null  :  defaultDescriptions.getProperty(key);
+			sb.append(key + "=" +  (desc != null ? desc : "") + "\n");
 		}
 		
 		return sb.toString();
--- src/test/java/edu/mayo/bior/cli/func/CreateCatalogPropsCommandITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/CreateCatalogPropsCommandITCase.java
@@ -1,15 +1,29 @@
 package edu.mayo.bior.cli.func;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.commons.cli.Option;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.mayo.bior.cli.cmd.CreateCatalogPropsCommand;
+import edu.mayo.cli.InvalidDataException;
+import edu.mayo.cli.InvalidOptionArgValueException;
+import edu.mayo.pipes.util.PropertiesFileUtil;
 
 public class CreateCatalogPropsCommandITCase extends BaseFunctionalTest {
 	
+	private File mDsPropsFile  = new File("src/test/resources/genes.datasource.properties");
+    private File mColPropsFile = new File("src/test/resources/genes.columns.properties");
+
+	private File mTestJsonDepthDsPropsFile  = new File("src/test/resources/metadata/testJsonDepth.datasource.properties");
+    private File mTestJsonDepthColPropsFile = new File("src/test/resources/metadata/testJsonDepth.columns.properties");
+
 	@Test
 	public void testCommand() throws IOException, InterruptedException {
 		String stdin = "src/test/resources/genes.tsv.bgz";
@@ -19,13 +33,90 @@ public class CreateCatalogPropsCommandITCase extends BaseFunctionalTest {
         assertEquals("", out.stderr);
         
         //the above command should create this file
-        File dsFile = new File("src/test/resources/genes.datasource.properties");
-        assertTrue(dsFile.exists());
-        dsFile.deleteOnExit();
+        Properties dsPropsActual   = new PropertiesFileUtil(mDsPropsFile.getCanonicalPath()).getProperties();
+        Properties dsPropsExpected = new PropertiesFileUtil("src/test/resources/genes.datasource.properties.expected").getProperties();
+        assertPropertiesSame(dsPropsExpected, dsPropsActual);
         
         //the above command should create this file
-        File cpFile = new File("src/test/resources/genes.columns.properties");
-        assertTrue(cpFile.exists());
-        cpFile.deleteOnExit();
+        Properties colPropsActual   = new PropertiesFileUtil(mColPropsFile.getCanonicalPath()).getProperties();
+        Properties colPropsExpected = new PropertiesFileUtil("src/test/resources/genes.columns.properties.expected").getProperties();
+        assertPropertiesSame(colPropsExpected, colPropsActual);
+	}
+
+	@Test
+	public void testNoCommand() throws IOException, InterruptedException, InvalidOptionArgValueException, InvalidDataException {
+		String catalog = "src/test/resources/genes.tsv.bgz";
+
+		CreateCatalogPropsCommand creator = new CreateCatalogPropsCommand();
+		Option opt = new Option("d", "catalog path");
+		creator.execNoCmd(catalog, opt);
+        
+        //the above command should create this file
+        Properties dsPropsActual   = new PropertiesFileUtil(mDsPropsFile.getCanonicalPath()).getProperties();
+        Properties dsPropsExpected = new PropertiesFileUtil("src/test/resources/genes.datasource.properties.expected").getProperties();
+        assertPropertiesSame(dsPropsExpected, dsPropsActual);
+        
+        //the above command should create this file
+        Properties colPropsActual   = new PropertiesFileUtil(mColPropsFile.getCanonicalPath()).getProperties();
+        Properties colPropsExpected = new PropertiesFileUtil("src/test/resources/genes.columns.properties.expected").getProperties();
+        assertPropertiesSame(colPropsExpected, colPropsActual);
+	}
+
+	@Test
+	public void testNoCmd_DeepJson() throws IOException, InterruptedException, InvalidOptionArgValueException, InvalidDataException {
+		String catalog = "src/test/resources/metadata/testJsonDepth.tsv.bgz";
+
+		CreateCatalogPropsCommand creator = new CreateCatalogPropsCommand();
+		Option opt = new Option("d", "catalog path");
+		creator.execNoCmd(catalog, opt);
+        
+        //the above command should create this file
+        Properties dsPropsActual   = new PropertiesFileUtil(mTestJsonDepthDsPropsFile.getCanonicalPath()).getProperties();
+        Properties dsPropsExpected = new PropertiesFileUtil("src/test/resources/metadata/testJsonDepth.datasource.properties.expected").getProperties();
+        assertPropertiesSame(dsPropsExpected, dsPropsActual);
+        
+        //the above command should create this file
+        Properties colPropsActual   = new PropertiesFileUtil(mTestJsonDepthColPropsFile.getCanonicalPath()).getProperties();
+        Properties colPropsExpected = new PropertiesFileUtil("src/test/resources/metadata/testJsonDepth.columns.properties.expected").getProperties();
+        assertPropertiesSame(colPropsExpected, colPropsActual);
+	}
+
+
+	@Before
+	public void beforeEachTest() {
+		removeTempPropFiles();
+	}
+
+	@After
+	public void afterEachTest() {
+		removeTempPropFiles();
+	}
+	
+	private void removeTempPropFiles() {
+        mDsPropsFile.delete();
+        mColPropsFile.delete();
+        
+        mTestJsonDepthDsPropsFile.delete();
+        mTestJsonDepthColPropsFile.delete();
+	}
+	
+	private void assertPropertiesSame(Properties expected, Properties actual) {
+		String[] keysExpected = expected.keySet().toArray(new String[0]);
+		String[] keysActual   = actual.keySet().toArray(new String[0]);
+		assertEquals("Size of properties are not the same", keysExpected.length, keysActual.length);
+		
+		// Validate that all keys are the same
+		for(int i=0; i < keysActual.length; i++) {
+			assertEquals("Key[" + i + "] not same (expected=" + keysExpected[i] + ", actual=" + keysActual[i] + ")",
+				keysExpected[i], keysActual[i]);
+		}
+		
+		// Validate all values are the same
+		for(int i=0; i < keysActual.length; i++) {
+			String key = keysActual[i];
+			assertEquals("Values[" + i + "] for key [" + key + "] don't match (expected=" 
+				+ expected.getProperty(key) + ", actual=" + actual.getProperty(key) + ")",
+				expected.getProperty(key), actual.getProperty(key)); 
+		}
 	}
 }
