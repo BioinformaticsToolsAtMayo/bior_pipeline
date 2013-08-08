46440108e89549704145911a9b58024d238c029e
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-08 15:42:58 +0000

    Split mashup VEPITCase into 3 separate test classes: VEPCommandITCase, VEPEXEITcase, and VEPPipelineITCase.  Each now corresponds 1-to-1 with the class they're testing.  Also updated based on new ##BIOR metdata header lines.

--- src/test/java/edu/mayo/bior/cli/func/remoteexec/helpers/RemoteFunctionalTest.java
+++ src/test/java/edu/mayo/bior/cli/func/remoteexec/helpers/RemoteFunctionalTest.java
@@ -30,7 +30,7 @@ public class RemoteFunctionalTest extends BaseFunctionalTest {
 	/** NOTE: All tests must be listed here if they are to be run!!!! */
 	private Class[] REMOTE_TESTS = {
 		edu.mayo.bior.cli.func.remoteexec.SNPEffITCase.class,
-		edu.mayo.bior.cli.func.remoteexec.VEPITCase.class, 
+		edu.mayo.bior.cli.func.remoteexec.VEPCommandITCase.class, 
 		edu.mayo.bior.cli.func.remoteexec.TreatITCase.class,
 		edu.mayo.bior.cli.func.remoteexec.ManyCmdsITCase.class
 	};
