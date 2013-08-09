11f84c5feb7d4eae2fedfec9e668a8e2976ef15c
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-09 14:42:07 +0000

    Fixing tab_to_tjson regression, and commiting test for it

--- src/test/java/edu/mayo/bior/cli/cmd/Tab2JSONCommandTest.java
+++ src/test/java/edu/mayo/bior/cli/cmd/Tab2JSONCommandTest.java
@@ -7,9 +7,11 @@ import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
 import edu.mayo.pipes.history.HistoryInPipe;
 import edu.mayo.pipes.history.HistoryOutPipe;
+import edu.mayo.pipes.util.metadata.Metadata;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 /**
@@ -29,21 +31,35 @@ public class Tab2JSONCommandTest {
     }
 
 
+    public final List<String> out = Arrays.asList(
+            "##BIOR=<ID=\"bior.ToTJson\",Operation=\"bior_tab_to_json\",DataType=\"JSON\",ShortUniqueName=\"ToTJson\">",
+            "#gene\trsID\tbior.ToTJson",
+            "MRPL39\trs142513484\t{\"gene\":\"MRPL39\",\"rsID\":\"rs142513484\"}",
+            "MRPL39\trs200187839\t{\"gene\":\"MRPL39\",\"rsID\":\"rs200187839\"}",
+            "PANX2\trs191258266\t{\"gene\":\"PANX2\",\"rsID\":\"rs191258266\"}",
+            "PANX2\trs145036669\t{\"gene\":\"PANX2\",\"rsID\":\"rs145036669\"}",
+            "BRCA1\trs184237074\t{\"gene\":\"BRCA1\",\"rsID\":\"rs184237074\"}",
+            "BRCA1\trs189382442\t{\"gene\":\"BRCA1\",\"rsID\":\"rs189382442\"}",
+            "BRCA1\trs182218567\t{\"gene\":\"BRCA1\",\"rsID\":\"rs182218567\"}"
+    );
+    
     @Test
     public void testConvertTab2JSON(){
         Tab2JSONCommand t = new Tab2JSONCommand();
+        Metadata md = new Metadata("bior_tab_to_json");
         Injector[] injectors = t.parseConfigFile("src/test/resources/Tab2JSONFiles/example.config");
         InjectIntoJsonPipe inject = new InjectIntoJsonPipe(true, injectors);
         Pipeline pipe = new Pipeline(
                 new CatPipe(),
-                new HistoryInPipe(),
+                new HistoryInPipe(md),
                 inject,
-                new HistoryOutPipe(),
-                new PrintPipe()
+                new HistoryOutPipe()
+                //new PrintPipe()
         );
         pipe.setStarts(Arrays.asList("src/test/resources/Tab2JSONFiles/example.tsv"));
-        while(pipe.hasNext()){
-            pipe.next();
+        for(int i = 0; out.size() > i; i++){
+            String s = (String) pipe.next();
+            assertEquals(out.get(i), s);
         }
     }
 
