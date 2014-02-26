51acb3ac399b7bee0b6c79e620a56581e7b08680
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2014-02-26 23:55:02 +0000

    DrillITCase.java: Simplified existing test cases; Added several test cases to check the negative, positive columns with -c flag with multiple drill paths, as well as using the -k flag

--- src/test/java/edu/mayo/bior/cli/func/DrillITCase.java
+++ src/test/java/edu/mayo/bior/cli/func/DrillITCase.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+
+import edu.mayo.pipes.util.test.PipeTestUtils;
 
 public class DrillITCase extends BaseFunctionalTest {
 
@@ -19,9 +22,17 @@ public class DrillITCase extends BaseFunctionalTest {
 			"#bior.JSON_COL" + "\n" + 
 			"{" +
 					"\"key1\":\"string_value1\"," +
-					"\"key2\": true," +
-					"\"key3\": 1" +
+					"\"key2\":true," +
+					"\"key3\":1" +
 			"}";	
+	
+	
+	private final String[] headerMeta = {
+	  prevCmdHeader,
+      "##BIOR=<ID=\"bior.JSON_COL.key1\",Operation=\"bior_drill\",Field=\"key1\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+	  "##BIOR=<ID=\"bior.JSON_COL.key2\",Operation=\"bior_drill\",Field=\"key2\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+	  "##BIOR=<ID=\"bior.JSON_COL.key3\",Operation=\"bior_drill\",Field=\"key3\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+	};
 	
 	@Test
 	public void testNormalPath() throws IOException, InterruptedException {
@@ -31,25 +42,13 @@ public class DrillITCase extends BaseFunctionalTest {
 		assertEquals(out.stderr, 0, out.exit);
 		assertEquals("", out.stderr);
 
-		String header = getHeader(out.stdout);
-        String[] headerLines = header.split("\n");
-        System.out.print(Arrays.asList(headerLines));
-        assertEquals(5, headerLines.length);
-        assertEquals(prevCmdHeader, headerLines[0]);		
-		assertEquals("##BIOR=<ID=\"bior.JSON_COL.key1\",Operation=\"bior_drill\",Field=\"key1\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">", headerLines[1]);
-		assertEquals("##BIOR=<ID=\"bior.JSON_COL.key2\",Operation=\"bior_drill\",Field=\"key2\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">", headerLines[2]);
-		assertEquals("##BIOR=<ID=\"bior.JSON_COL.key3\",Operation=\"bior_drill\",Field=\"key3\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">", headerLines[3]);
-		assertEquals("#bior.JSON_COL.key1\tbior.JSON_COL.key2\tbior.JSON_COL.key3", headerLines[4]);
-
-		// pull out just data rows		
-		String data = out.stdout.replace(header, "");		
-		String[] cols = data.split("\t");
-		
-		assertEquals(3, cols.length);
-
-        assertEquals("string_value1",	cols[0].trim());
-        assertEquals("true",			cols[1].trim());
-        assertEquals("1",				cols[2].trim());
+		List<String> actual = Arrays.asList(out.stdout.split("\n"));
+		List<String> expected = Arrays.asList(
+				headerMeta[0], headerMeta[1], headerMeta[2], headerMeta[3], 
+				"#bior.JSON_COL.key1\tbior.JSON_COL.key2\tbior.JSON_COL.key3",
+				"string_value1\ttrue\t1"
+				);
+		PipeTestUtils.assertListsEqual(expected, actual);
 	}	
 
 	@Test
@@ -60,28 +59,13 @@ public class DrillITCase extends BaseFunctionalTest {
 		assertEquals(out.stderr, 0, out.exit);
 		assertEquals("", out.stderr);
 
-		System.out.println(stdin);
-		
-		String header = getHeader(out.stdout);
-        String[] headerLines = header.split("\n");
-        assertEquals(3, headerLines.length);
-        assertEquals(prevCmdHeader, headerLines[0]);		
-		assertEquals("##BIOR=<ID=\"bior.JSON_COL.key3\",Operation=\"bior_drill\",Field=\"key3\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">", headerLines[1]);
-		assertEquals("#bior.JSON_COL.key3	bior.JSON_COL", headerLines[2]);
-
-		// pull out just data rows
-		String data = out.stdout.replace(header, "");		
-		
-		// pull out just json
-		String expectedJson = stdin.replace(getHeader(stdin), "");
-		
-		// JSON should be added as last column (4th)
-		String[] cols = data.split("\t");
-		
-		assertEquals(2, cols.length);
-
-        assertEquals("1",				cols[0].trim());
-        assertEquals(expectedJson,		cols[1].trim());
+		List<String> actual = Arrays.asList(out.stdout.split("\n"));
+		List<String> expected = Arrays.asList(
+				headerMeta[0], headerMeta[3], 
+				"#bior.JSON_COL.key3\tbior.JSON_COL",
+				"1\t{\"key1\":\"string_value1\",\"key2\":true,\"key3\":1}"
+				);
+		PipeTestUtils.assertListsEqual(expected, actual);
 	}
 	
 	@Test
@@ -97,9 +81,9 @@ public class DrillITCase extends BaseFunctionalTest {
 				prevCmdHeader + "\n" +			
 				"#bior.dbSNP137" + "\n" + 
 				"{" +
-					"\"INFO\": {" +
-						"\"RSPOS\": 10145," +
-						"\"dbSNPBuildID\": 134" +
+					"\"INFO\":{" +
+						"\"RSPOS\":10145," +
+						"\"dbSNPBuildID\":134" +
 						"}" +
 				"}";	
 				
@@ -107,22 +91,98 @@ public class DrillITCase extends BaseFunctionalTest {
 		assertEquals(out.stderr, 0, out.exit);
 		assertEquals("", out.stderr);
 
-		String header = getHeader(out.stdout);
-		System.out.println(header);
-        String[] headerLines = header.split("\n");
-        assertEquals(4, headerLines.length);
-        assertEquals(prevCmdHeader, headerLines[0]);		
-		assertEquals(String.format("##BIOR=<ID=\"bior.dbSNP137.INFO.RSPOS\",Operation=\"bior_drill\",Field=\"INFO.RSPOS\",DataType=\"Integer\",Number=\"1\",FieldDescription=\"Chromosome position reported in dbSNP\",ShortUniqueName=\"dbSNP137\",Source=\"dbSNP\",Version=\"137\",Build=\"GRCh37.p10\",Path=\"%s\">", catCanonicalPath), headerLines[1]);
-		assertEquals(String.format("##BIOR=<ID=\"bior.dbSNP137.INFO.dbSNPBuildID\",Operation=\"bior_drill\",Field=\"INFO.dbSNPBuildID\",DataType=\"Integer\",Number=\"1\",FieldDescription=\"First dbSNP build for RS\",ShortUniqueName=\"dbSNP137\",Source=\"dbSNP\",Version=\"137\",Build=\"GRCh37.p10\",Path=\"%s\">", catCanonicalPath), headerLines[2]);
-		assertEquals("#bior.dbSNP137.INFO.RSPOS\tbior.dbSNP137.INFO.dbSNPBuildID", headerLines[3]);
+		List<String> actual = Arrays.asList(out.stdout.split("\n"));
+		List<String> expected=Arrays.asList(
+				prevCmdHeader,
+				String.format("##BIOR=<ID=\"bior.dbSNP137.INFO.RSPOS\",Operation=\"bior_drill\",Field=\"INFO.RSPOS\",DataType=\"Integer\",Number=\"1\",FieldDescription=\"Chromosome position reported in dbSNP\",ShortUniqueName=\"dbSNP137\",Source=\"dbSNP\",Version=\"137\",Build=\"GRCh37.p10\",Path=\"%s\">", catCanonicalPath),
+				String.format("##BIOR=<ID=\"bior.dbSNP137.INFO.dbSNPBuildID\",Operation=\"bior_drill\",Field=\"INFO.dbSNPBuildID\",DataType=\"Integer\",Number=\"1\",FieldDescription=\"First dbSNP build for RS\",ShortUniqueName=\"dbSNP137\",Source=\"dbSNP\",Version=\"137\",Build=\"GRCh37.p10\",Path=\"%s\">", catCanonicalPath),
+				"#bior.dbSNP137.INFO.RSPOS\tbior.dbSNP137.INFO.dbSNPBuildID",
+				"10145\t134"
+				);
+		PipeTestUtils.assertListsEqual(expected, actual);
+	}	
+	
+	
+	@Test
+    /** Test multiple JSON paths drilled, positive column # for the JSON */
+    public void testMultiPath_PosCol() throws IOException, InterruptedException {
+        System.out.println("DrillITCase.testMultiPath_PosCol(): Test multiple JSON paths drilled, specifying a positive column number");
+        
+        
+    	final String STDIN = prevCmdHeader + "\n"
+        			+ "#CHROM\tPOS\tbior.JSON_COL\tINFO\n"
+        			+ "1\t100\t{\"key1\":11,\"key2\":22,\"key3\":33}\tsomeInfo";	
 
-		// pull out just data rows		
-		String data = out.stdout.replace(header, "");		
-		String[] cols = data.split("\t");
-		
-		assertEquals(2, cols.length);
+        CommandOutput out = executeScript("bior_drill", STDIN, "-p", "key1", "-p", "key2", "-p", "key3", "-c", "3", "--log");
 
-        assertEquals("10145",	cols[0].trim());
-        assertEquals("134",		cols[1].trim());
+		assertEquals(out.stderr, 0, out.exit);
+		assertEquals("", out.stderr);
+
+		List<String> actual = Arrays.asList(out.stdout.split("\n"));
+		List<String> expected = Arrays.asList(
+				prevCmdHeader,
+				"##BIOR=<ID=\"bior.JSON_COL.key1\",Operation=\"bior_drill\",Field=\"key1\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"##BIOR=<ID=\"bior.JSON_COL.key2\",Operation=\"bior_drill\",Field=\"key2\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"##BIOR=<ID=\"bior.JSON_COL.key3\",Operation=\"bior_drill\",Field=\"key3\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"#CHROM\tPOS\tINFO\tbior.JSON_COL.key1\tbior.JSON_COL.key2\tbior.JSON_COL.key3",
+				"1\t100\tsomeInfo\t11\t22\t33"
+				);
+		PipeTestUtils.assertListsEqual(expected, actual);
+	}	
+
+	@Test
+    /** Test multiple JSON paths drilled, negative column # for the JSON */
+    public void testMultiPath_NegCol() throws IOException, InterruptedException {
+        System.out.println("DrillITCase.testMultiPath_NegCol(): Test multiple JSON paths drilled, specifying a negative column number");
+        
+        
+    	final String STDIN = prevCmdHeader + "\n"
+        			+ "#CHROM\tPOS\tbior.JSON_COL\tINFO\n"
+        			+ "1\t100\t{\"key1\":11,\"key2\":22,\"key3\":33}\tsomeInfo";	
+
+        CommandOutput out = executeScript("bior_drill", STDIN, "-p", "key1", "-p", "key2", "-p", "key3", "-c", "-2", "--log");
+
+		assertEquals(out.stderr, 0, out.exit);
+		assertEquals("", out.stderr);
+
+		List<String> actual = Arrays.asList(out.stdout.split("\n"));
+		List<String> expected = Arrays.asList(
+				prevCmdHeader,
+				"##BIOR=<ID=\"bior.JSON_COL.key1\",Operation=\"bior_drill\",Field=\"key1\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"##BIOR=<ID=\"bior.JSON_COL.key2\",Operation=\"bior_drill\",Field=\"key2\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"##BIOR=<ID=\"bior.JSON_COL.key3\",Operation=\"bior_drill\",Field=\"key3\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"#CHROM\tPOS\tINFO\tbior.JSON_COL.key1\tbior.JSON_COL.key2\tbior.JSON_COL.key3",
+				"1\t100\tsomeInfo\t11\t22\t33"
+				);
+		PipeTestUtils.assertListsEqual(expected, actual);
+
+	}
+	
+	@Test
+    /** Test multiple JSON paths drilled, negative column # for the JSON, and keep the JSON column (which will remove it from its current position and append it on the end) */
+    public void testMultiPath_NegCol_keepJson() throws IOException, InterruptedException {
+        System.out.println("DrillITCase.testMultiPath_NegCol_keepJson(): Test multiple JSON paths drilled, specifying a negative column number, and keeping the JSON column");
+        
+        
+    	final String STDIN = prevCmdHeader + "\n"
+        			+ "#CHROM\tPOS\tbior.JSON_COL\tINFO\n"
+        			+ "1\t100\t{\"key1\":11,\"key2\":22,\"key3\":33}\tsomeInfo";	
+
+        CommandOutput out = executeScript("bior_drill", STDIN, "-p", "key1", "-p", "key2", "-p", "key3", "-c", "-2", "-k", "--log");
+
+		assertEquals(out.stderr, 0, out.exit);
+		assertEquals("", out.stderr);
+
+		List<String> actual = Arrays.asList(out.stdout.split("\n"));
+		List<String> expected = Arrays.asList(
+				prevCmdHeader,
+				"##BIOR=<ID=\"bior.JSON_COL.key1\",Operation=\"bior_drill\",Field=\"key1\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"##BIOR=<ID=\"bior.JSON_COL.key2\",Operation=\"bior_drill\",Field=\"key2\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"##BIOR=<ID=\"bior.JSON_COL.key3\",Operation=\"bior_drill\",Field=\"key3\",DataType=\"String\",Number=\".\",FieldDescription=\"\",ShortUniqueName=\"JSON_COL\",Path=\"/path/to/catalog\">",
+				"#CHROM\tPOS\tINFO\tbior.JSON_COL.key1\tbior.JSON_COL.key2\tbior.JSON_COL.key3\tbior.JSON_COL",
+				"1\t100\tsomeInfo\t11\t22\t33\t{\"key1\":11,\"key2\":22,\"key3\":33}"
+				);
+		PipeTestUtils.assertListsEqual(expected, actual);
+
 	}	
 }
